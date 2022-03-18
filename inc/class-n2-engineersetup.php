2c7c3a7c26dac102d7994859a725d61da30153ad
KouWakai <kouyama112@gmail.com>
2022-03-18 15:46:29 +0900

    ハードコーディングしてダッシュボードにウィジェットを追加する方法と雰囲気を掴んだ

--- inc/class-n2-engineersetup.php
+++ inc/class-n2-engineersetup.php
@@ -22,19 +22,29 @@ class N2_Engineersetup {
 	 * コンストラクタ
 	 */
 	public function __construct() {
-		add_action( 'admin_notices', array( $this, 'hoge' ) );
+		add_action( 'wp_dashboard_setup', array( $this, 'add_widgets' ) );
 	}
 
-	/**
-	 * hoge
-	 */
-	public function addwidegets() {
-		$message = array(
-			'hello',
-			'world',
-			'!!!',
-		);
-		$message = apply_filters( 'n2_hello', $message );
-		echo implode( ' ', $message );
+	public function add_widgets() {
+		wp_add_dashboard_widget('contact_setup_widget', '事業者連絡先', array($this,'contact_setup_widget'));
+	}
+
+    function contact_setup_widget(){
+		?>
+		<form>
+			<p>事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。</p>
+			<input type="hidden" name="action" value="<?=NENG_DB_TABLENAME?>">
+			<input type="hidden" name="judge" value="option">
+			<p class="input-text-wrap">
+				メールアドレス：
+				<input type="text" name="<?=NENG_DB_TABLENAME?>[contact][email]" value="<?=NENG_OPTION['contact']['email']?>">
+			</p>
+			<p class="input-text-wrap">
+				電話番号：
+				<input type="text" name="<?=NENG_DB_TABLENAME?>[contact][tel]" value="<?=NENG_OPTION['contact']['tel']?>">
+			</p>
+			<input type="submit" class="button button-primary sissubmit" value="　更新する　">
+		</form>
+		<?php
 	}
 }
