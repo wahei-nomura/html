4e3e57851f34394e06012e08cbc3fce9dc2a5616
KouWakai <kouyama112@gmail.com>
2022-04-08 16:45:46 +0900

    イテレータで回すとこまでできた

--- inc/class-n2-engineersetup.php
+++ inc/class-n2-engineersetup.php
@@ -24,7 +24,7 @@ class N2_Engineersetup {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_setup_menu' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'setup_menu_style' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'setup_menu_style' ) );
 	}
 
 	/**
@@ -35,41 +35,63 @@ class N2_Engineersetup {
 		add_menu_page( '各種セットアップ', '各種セットアップ', 'manage_options', 'setup_menu', array( $this, 'add_setup_menu_page' ), 'dashicons-list-view' );
 	}
 
-	/**
-	 public function add_widgets() {
-	 $widgets = array(
-	  array(
-	   "widget_name" => '事業者連絡先',
-	   "description" => "事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。",
-	   "input1" => array("メールアドレス","email","contact"),
-	   "input2" => array("電話番号","tel","contact"),
-	  ),
-	  array(
-	   "widget_name" => '各ポータル共通説明文',
-	   "text1" => array("商品説明文の文末に追加したいテキスト","<?=get_bloginfo('name')?>","add_text"),
-	  ),
-	 );
-	 foreach($widgets as $v){
-	  wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v[0]);
-	  wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v)[1];
-	 }
-	}
-	 */
-
 	 /**
 	  * メニュー描画
 	  *
 	  * @return void
 	  */
 	public function add_setup_menu_page() {
+		$menus = array(
+			array(
+				'menu_name'   => '事業者連絡先',
+				'description' => '事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。',
+				'input1'      => array( 'メールアドレス', 'email', 'contact' ),
+				'input2'      => array( '電話番号', 'tel', 'contact' ),
+			),
+			array(
+				'menu_name' => '各ポータル共通説明文',
+				'text1'     => array( '商品説明文の文末に追加したいテキスト', "<?=get_bloginfo('name')?>", 'add_text' ),
+			),
+		);
 		?>
 		<div class="wrap">
-			<h2>各種セットアップ</h2>
+		<?php
+		foreach ( $menus as $menu ) {
+			?>
 			<div class="row">
-				<h2>事業者連絡先</h2>
-				<div>事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。</div>
-				<div>メールアドレス</div>
+			<h2><?php echo $menu['menu_name']; ?></h2>
+			<?php
+			if ( array_key_exists( 'description', $menu ) ) {
+				echo '<p>' . $menu['description'] . '</p>';
+			}
+			?>
+			<div class="flex">
+			<?php
+			foreach ( $menu as $v ) {
+				switch ( array_search( $v, $menu ) ) {
+					case preg_match( '/input[1-9]/u', array_search( $v, $menu ) ) === 1:
+						?>
+						<div class="column">
+							<p><?php echo $v[0]; ?>：</p>
+							<input type="text" name="<?php echo NENG_DB_TABLENAME; ?>[<?php echo $v[2]; ?>][<?php echo $v[1]; ?>]" value="<?php echo NENG_OPTION['<?php echo $v[2]?>']['<?php echo $v[1]?>']; ?>">
+						</div>
+						<?php
+						break;
+					case preg_match( '/text[1-9]/u', array_search( $v, $menu ) ) === 1:
+						?>
+						<div class="column">
+							<p><?php echo $v[0]; ?>：</p>
+							<textarea name="<?php echo NENG_DB_TABLENAME; ?>[<?php echo $v[2]; ?>][<?php echo get_bloginfo( 'name' ); ?>]" rows="7" style="overflow-x: hidden;"><?php echo NENG_OPTION['<?php echo $v[2]?>'][ get_bloginfo( 'name' ) ]; ?></textarea>
+						</div>
+						<?php
+				}
+			}
+			?>
+				</div>
 			</div>
+			<?php
+		}
+		?>
 		</div>
 		<?php
 	}
@@ -80,43 +102,6 @@ class N2_Engineersetup {
 	 * @return void
 	 */
 	public function setup_menu_style() {
-		wp_enqueue_style( 'n2-setupmenu', get_template_directory_uri() . '/dist/setupmenu.css', array(), wp_get_theme()->get( 'Version' ), true );
+		wp_enqueue_style( 'n2-setupmenu', get_template_directory_uri() . '/dist/setupmenu.css', array(), wp_get_theme()->get( 'Version' ) );
 	}
-
-	/**
-	 function setup_widget( $var, $args ){
-	?>
-	<form>
-	<input type="hidden" name="action" value="<?=NENG_DB_TABLENAME?>">
-	<input type="hidden" name="judge" value="option">
-	<?php
-	foreach($args[args] as $v){
-	switch(array_search($v,$args[args])){
-				case "description":
-					?>
-					<p><?php echo $v ?></p>
-					<?php
-					break;
-				case preg_match("/input[1-9]/u", array_search($v,$args[args])) === 1 :
-					?>
-					<p class="input-text-wrap">
-						<?php echo $v[0]?>：
-						<input type="text" name="<?=NENG_DB_TABLENAME?>[<?php echo $v[2]?>][<?php echo $v[1]?>]" value="<?=NENG_OPTION['<?php echo $v[2]?>']['<?php echo $v[1]?>']?>">
-					</p>
-					<?php
-					break;
-				case preg_match("/text[1-9]/u", array_search($v,$args[args])) === 1 :
-					?>
-					<p class="textarea-wrap">
-						<?php echo $v[0]?>：
-						<textarea name="<?=NENG_DB_TABLENAME?>[<?php echo $v[2]?>][<?=get_bloginfo('name')?>]" rows="7" style="overflow-x: hidden;"><?=NENG_OPTION['<?php echo $v[2]?>'][get_bloginfo('name')]?></textarea>
-					</p>
-					<?php
-	}
-	}
-	?>
-	<input type="submit" class="button button-primary sissubmit" value="　更新する　">
-	</form>
-	<?php
-	} */
 }
