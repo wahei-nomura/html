35ac139a991394515812d78d1ef3737c2cb124df
KouWakai <kouyama112@gmail.com>
2022-04-08 13:34:35 +0900

    css読み込む設定を追加した

--- inc/class-n2-engineersetup.php
+++ inc/class-n2-engineersetup.php
@@ -18,56 +18,80 @@ if ( class_exists( 'N2_Engineersetup' ) ) {
  * Hogehoge
  */
 class N2_Engineersetup {
+
 	/**
 	 * コンストラクタ
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_setup_menu' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'setup_menu_style' ) );
 	}
 
-	public function add_setup_menu(){
-		add_menu_page('各種セットアップ', '各種セットアップ', 'manage_options', 'setup_menu', array($this,'add_setup_menu_page'),'dashicons-list-view');
+	/**
+	 * add_setup_menu
+	 * セットアップ管理ページを追加
+	 */
+	public function add_setup_menu() {
+		add_menu_page( '各種セットアップ', '各種セットアップ', 'manage_options', 'setup_menu', array( $this, 'add_setup_menu_page' ), 'dashicons-list-view' );
 	}
 
-/* 	public function add_widgets() {
-		$widgets = array(
-			array(
-				"widget_name" => '事業者連絡先',
-				"description" => "事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。",
-				"input1" => array("メールアドレス","email","contact"),
-				"input2" => array("電話番号","tel","contact"),
-			),
-			array(
-				"widget_name" => '各ポータル共通説明文',
-				"text1" => array("商品説明文の文末に追加したいテキスト","<?=get_bloginfo('name')?>","add_text"),
-			),
-		);
-		foreach($widgets as $v){
-			wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v[0]);
-			wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v)[1];
-		}
-	} */
+	/**
+	 public function add_widgets() {
+	 $widgets = array(
+	  array(
+	   "widget_name" => '事業者連絡先',
+	   "description" => "事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。",
+	   "input1" => array("メールアドレス","email","contact"),
+	   "input2" => array("電話番号","tel","contact"),
+	  ),
+	  array(
+	   "widget_name" => '各ポータル共通説明文',
+	   "text1" => array("商品説明文の文末に追加したいテキスト","<?=get_bloginfo('name')?>","add_text"),
+	  ),
+	 );
+	 foreach($widgets as $v){
+	  wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v[0]);
+	  wp_add_dashboard_widget('setup_widget', $v[widget_name], array($this,'setup_widget'),null,$v)[1];
+	 }
+	}
+	 */
 
-	function add_setup_menu_page()
-	{
+	 /**
+	  * メニュー描画
+	  *
+	  * @return void
+	  */
+	public function add_setup_menu_page() {
 		?>
-			<div class="wrap">
-				<h2>各種セットアップ</h2>
-					<div class="row">
-						<h2>事業者連絡先</h2>
-					</div>
+		<div class="wrap">
+			<h2>各種セットアップ</h2>
+			<div class="row">
+				<h2>事業者連絡先</h2>
+				<div>事業者さまとのやりとりに使用するメールアドレス・電話番号を記入してください。</div>
+				<div>メールアドレス</div>
 			</div>
+		</div>
 		<?php
 	}
 
-    /* function setup_widget( $var, $args ){
-		?>
-		<form>
-		<input type="hidden" name="action" value="<?=NENG_DB_TABLENAME?>">
-		<input type="hidden" name="judge" value="option">
-		<?php
-		foreach($args[args] as $v){
-			switch(array_search($v,$args[args])){
+	/**
+	 * このクラスで使用するassetsの読み込み
+	 *
+	 * @return void
+	 */
+	public function setup_menu_style() {
+		wp_enqueue_style( 'n2-setupmenu', get_template_directory_uri() . '/dist/setupmenu.css', array(), wp_get_theme()->get( 'Version' ), true );
+	}
+
+	/**
+	 function setup_widget( $var, $args ){
+	?>
+	<form>
+	<input type="hidden" name="action" value="<?=NENG_DB_TABLENAME?>">
+	<input type="hidden" name="judge" value="option">
+	<?php
+	foreach($args[args] as $v){
+	switch(array_search($v,$args[args])){
 				case "description":
 					?>
 					<p><?php echo $v ?></p>
@@ -88,11 +112,11 @@ class N2_Engineersetup {
 						<textarea name="<?=NENG_DB_TABLENAME?>[<?php echo $v[2]?>][<?=get_bloginfo('name')?>]" rows="7" style="overflow-x: hidden;"><?=NENG_OPTION['<?php echo $v[2]?>'][get_bloginfo('name')]?></textarea>
 					</p>
 					<?php
-			}
-		}
-		?>
-		<input type="submit" class="button button-primary sissubmit" value="　更新する　">
-		</form>
-		<?php
+	}
+	}
+	?>
+	<input type="submit" class="button button-primary sissubmit" value="　更新する　">
+	</form>
+	<?php
 	} */
 }
