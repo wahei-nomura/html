6a89c4e474c5e51f75369a3bfed59ef00c2af697
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-06-14 14:38:46 +0900

    途中だけど一旦コミット

--- inc/class-n2-item-export.php
+++ inc/class-n2-item-export.php
@@ -45,6 +45,8 @@ class N2_Item_Export {
 	 * @return void
 	 */
 	public function ledghome() {
+		// itemの情報を配列か
+		$items_arr  = array();
 		$header_str = parse_ini_file( get_template_directory() . '/config/n2-file-header.ini', true )['ledghome']['csv_header'];
 		$header     = explode( ',', $header_str );
 		$csv        = $header_str . PHP_EOL;
@@ -53,13 +55,21 @@ class N2_Item_Export {
 
 		foreach ( $ids as $id ) {
 			foreach ( $header as $head ) {
-				$csv .= ! empty( get_post_meta( $id, $head, true ) ) ? '"' . get_post_meta( $id, $head, true ) . '"' : '';
-				$csv .= ',';
+				$items_arr[ $id ][ $head ]  = ! empty( get_post_meta( $id, $head, true ) ) ? get_post_meta( $id, $head, true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品名' === $head ? $items_arr[ $id ]['謝礼品番号'] . ' ' : '';
+				// $items_arr[ $id ][ $head ] .= '謝礼品名' === $head ? ( get_post_meta( $id, '略称', true ) ) ? get_post_meta( $id, '略称', true ) : $this->_s( get_the_title( $id ) );
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
+				$items_arr[ $id ][ $head ]  = '謝礼品番号' === $head ? get_post_meta( $id, '返礼品コード', true ) : '';
 			}
-			$csv  = substr( $csv, 0, -1 );
-			$csv .= PHP_EOL;
 		}
-
-		$this->download_data( $csv );
+		var_dump( $items_arr );
+		// $this->download_data( $csv );
 	}
 }
