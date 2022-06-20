a89b683a235a050ed1ed57bba7138fef7d91bfec
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-06-20 10:50:06 +0900

    プラグイン側でループ回さなくていいように調整

--- inc/class-n2-item-export.php
+++ inc/class-n2-item-export.php
@@ -88,34 +88,33 @@ class N2_Item_Export {
 					$items_arr[ $key_id ][ $head ] = ! empty( get_post_meta( $id, $head, true ) ) ? get_post_meta( $id, $head, true ) : '';
 				}
 
-				$items_arr[ $key_id ]['謝礼品番号']      = trim( strtoupper( get_post_meta( $id, '返礼品コード', true ) ) ) . $teikinum;
-				$items_arr[ $key_id ]['謝礼品名']       = $items_arr[ $key_id ]['謝礼品番号'] . ' ';
-				$items_arr[ $key_id ]['謝礼品名']      .= ( get_post_meta( $id, '略称', true ) ) ? get_post_meta( $id, '略称', true ) : N2_Functions::_s( get_the_title( $id ) );
-				$items_arr[ $key_id ]['事業者']        = get_the_author_meta( 'display_name', get_post_field( 'post_author', $id ) );
-				$items_arr[ $key_id ]['配送名称']       = ( get_post_meta( $id, '配送伝票表示名', true ) ) ? ( $items_arr[ $key_id ]['謝礼品番号'] . ' ' . get_post_meta( $id, '配送伝票表示名', true ) ) : $items_arr[ $key_id ]['謝礼品名'];
-				$items_arr[ $key_id ]['ふるさとチョイス名称'] = N2_Functions::_s( get_the_title( $id ) ) . " [{$items_arr[$key_id]['謝礼品番号']}]";
-				$items_arr[ $key_id ]['楽天名称']       = '【ふるさと納税】' . N2_Functions::_s( get_the_title( $id ) ) . " [{$items_arr[$key_id]['謝礼品番号']}]";
-				$items_arr[ $key_id ]['謝礼品カテゴリー']   = get_post_meta( $id, 'カテゴリー', true );
-				$items_arr[ $key_id ]['セット内容']      = N2_Functions::_s( get_post_meta( $id, '内容量・規格等', true ) );
-				$items_arr[ $key_id ]['謝礼品紹介文']     = N2_Functions::_s( get_post_meta( $id, '説明文', true ) );
-				$items_arr[ $key_id ]['ステータス']      = '受付中';
-				$items_arr[ $key_id ]['状態']         = '表示';
-				$items_arr[ $key_id ]['寄附設定金額']     = $i < 2 ? get_post_meta( $id, '寄附金額', true ) : 0;
-				// $arr[ $key_kid ]['価格（税込み）'] = ( $opt['ledghome']['teikiprice'] == true ) ? ( ( $i < 2 ) ? $price * $teiki : 0 ) : $price;
-				$items_arr[ $key_id ]['送料'] = get_post_meta( $id, '送料', true );
-				// $items_arr[ $key_id ]['送料反映'] = ( empty( $opt['ledghome']['souryouhanei'] ) ) ? '反映しない' : '反映する';
-				$items_arr[ $key_id ]['発送方法'] = get_post_meta( $id, '発送方法', true );
-				// 取り扱い方法保留
+				$item_num = trim( strtoupper( get_post_meta( $id, '返礼品コード', true ) ) ) . $teikinum;
 
-				$items_arr[ $key_id ]['申込可能期間']   = '通年';
-				$items_arr[ $key_id ]['自由入力欄1']   = date( 'Y/m/d' ) . '：' . wp_get_current_user()->display_name;
-				$items_arr[ $key_id ]['自由入力欄2']   = $items_arr[ $key_id ]['送料'];
-				$items_arr[ $key_id ]['配送サイズコード'] = ( is_numeric( get_post_meta( $id, '発送サイズ', true ) ) ) ? get_post_meta( $id, '発送サイズ', true ) : '';
+				$arr = array(
+					'謝礼品番号'     => $item_num,
+					'謝礼品名'      => $item_num . ' ' . ( get_post_meta( $id, '略称', true ) ? get_post_meta( $id, '略称', true ) : N2_Functions::_s( get_the_title( $id ) ) ),
+					'事業者'       => get_the_author_meta( 'display_name', get_post_field( 'post_author', $id ) ),
+					'配送名称'      => ( get_post_meta( $id, '配送伝票表示名', true ) ) ? ( $item_num . ' ' . get_post_meta( $id, '配送伝票表示名', true ) ) : $item_num,
+					'ふるさとチョイス名' => N2_Functions::_s( get_the_title( $id ) ) . " [{$item_num}]",
+					'楽天名称'      => '【ふるさと納税】' . N2_Functions::_s( get_the_title( $id ) ) . " [{$item_num}]",
+					'謝礼品カテゴリー'  => get_post_meta( $id, 'カテゴリー', true ),
+					'セット内容'     => N2_Functions::_s( get_post_meta( $id, '内容量・規格等', true ) ),
+					'謝礼品紹介文'    => N2_Functions::_s( get_post_meta( $id, '説明文', true ) ),
+					'ステータ'      => '受付中',
+					'状態'        => '表示',
+					'寄附設定金額'    => $i < 2 ? get_post_meta( $id, '寄附金額', true ) : 0,
+					'送料'        => get_post_meta( $id, '送料', true ),
+					'発送方法'      => get_post_meta( $id, '発送方法', true ),
+					'申込可能期間'    => '通年',
+					'自由入力欄1'    => date( 'Y/m/d' ) . '：' . wp_get_current_user()->display_name,
+					'自由入力欄2'    => get_post_meta( $id, '送料', true ),
+					'配送サイズコード'  => ( is_numeric( get_post_meta( $id, '発送サイズ', true ) ) ) ? get_post_meta( $id, '発送サイズ', true ) : '',
+				);
+
+				// 内容を追加、または上書きするためのフック
+				list( $id, $items_arr[ $key_id ] ) = apply_filters( 'n2_item_export_ledghome_items', array( $id, $arr ) );
 			}
 		}
-
-		// プラグイン側で項目を追加
-		list($ids, $items_arr) = apply_filters( 'n2_item_export_ledghome_items', array( $ids, $items_arr ) );
 
 		$this->download_csv( 'ledghome', $header, $items_arr, $csv_title );
 	}
