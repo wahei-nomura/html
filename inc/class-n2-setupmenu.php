6ee493ca65ce1189a2d2a177ccddad8c087c203d
hirotoki3193 <1stlevi777@gmail.com>
2022-07-11 14:38:40 +0900

    :bug:[fix] クルーセットアップのNoticeを非表示に
    NULL演算子で非表示化する

--- inc/class-n2-setupmenu.php
+++ inc/class-n2-setupmenu.php
@@ -47,8 +47,7 @@ class N2_Setupmenu {
 		$opt = get_option( $this->cls );
 		extract( $_POST );
 		$opt = array_merge( (array) $opt, ${$this->cls} );
-		update_option( $this->cls, $opt );
-		echo '登録完了';
+		echo update_option( $this->cls, $opt ) ? '登録完了' : '登録失敗';
 		die();
 	}
 	/**
@@ -66,7 +65,7 @@ class N2_Setupmenu {
 	public function add_crew_setup_menu_page() {
 		$this->wrapping_contents( '事業者連絡先', 'contact_setup_menu' );
 		$this->wrapping_contents( '各ポータル共通説明文', 'add_text_widget' );
-		// $this->wrapping_contents( '楽天セットアップ', 'rakuten_setup_widget' ); //必要？
+		$this->wrapping_contents( '楽天セットアップ', 'rakuten_setup_widget' ); // 必要？
 	}
 
 	/**
@@ -83,13 +82,13 @@ class N2_Setupmenu {
 			<p class="input-text-wrap">
 				<label>
 					メールアドレス：
-					<input type="text" name="<?php echo $this->cls; ?>[contact][email]" value="<?php echo get_option( $this->cls )['contact']['email']; ?>">
+					<input type="text" name="<?php echo $this->cls; ?>[contact][email]" value="<?php echo get_option( $this->cls )['contact']['email'] ?? ''; ?>">
 				</label>
 			</p>
 			<p class="input-text-wrap">
 				<label>
 					電話番号：
-					<input type="text" name="<?php echo $this->cls; ?>[contact][tel]" value="<?php echo get_option( $this->cls )['contact']['tel']; ?>">
+					<input type="text" name="<?php echo $this->cls; ?>[contact][tel]" value="<?php echo get_option( $this->cls )['contact']['tel'] ?? ''; ?>">
 				</label>
 			</p>
 			<input type="submit" class="button button-primary sissubmit" value="　更新する　">
@@ -109,7 +108,7 @@ class N2_Setupmenu {
 			<p class="textarea-wrap">
 				<label>
 					商品説明文の文末に追加したいテキスト：
-					<textarea name="<?php echo $this->cls; ?>[add_text][<?php echo get_bloginfo( 'name' ); ?>]" rows="7" style="overflow-x: hidden;"><?php echo get_option( $this->cls )['add_text'][ get_bloginfo( 'name' ) ]; ?></textarea>
+					<textarea name="<?php echo $this->cls; ?>[add_text][<?php echo get_bloginfo( 'name' ); ?>]" rows="7" style="overflow-x: hidden;"><?php echo get_option( $this->cls )['add_text'][ get_bloginfo( 'name' ) ] ?? ''; ?></textarea>
 				</label>
 			</p>
 			<input type="submit" class="button button-primary sissubmit" value="　更新する　">
@@ -126,21 +125,21 @@ class N2_Setupmenu {
 		<form>
 			<input type="hidden" name="action" value="<?php echo $this->cls; ?>">
 			<input type="hidden" name="judge" value="option">
-			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][ftp_server]" value="<?php echo get_option( $this->cls )['rakuten']['ftp_server']; ?>">
-			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][ftp_server_port]" value="<?php echo get_option( $this->cls )['rakuten']['ftp_server_port']; ?>">
-			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][upload_server]" value="<?php echo get_option( $this->cls )['rakuten']['upload_server']; ?>">
-			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][upload_server_port]" value="<?php echo get_option( $this->cls )['rakuten']['upload_server_port']; ?>">
+			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][ftp_server]" value="<?php echo get_option( $this->cls )['rakuten']['ftp_server'] ?? ''; ?>">
+			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][ftp_server_port]" value="<?php echo get_option( $this->cls )['rakuten']['ftp_server_port'] ?? ''; ?>">
+			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][upload_server]" value="<?php echo get_option( $this->cls )['rakuten']['upload_server'] ?? ''; ?>">
+			<input type="hidden" name="<?php echo $this->cls; ?>[rakuten][upload_server_port]" value="<?php echo ( get_option( $this->cls )['rakuten']['upload_server_port'] ?? '' ); ?>">
 			<p class="textarea-wrap">
 				<label>
 					説明文追加html：
-					<textarea name="<?php echo $this->cls; ?>[rakuten][html]" rows="5" style="overflow-x: hidden;"><?php echo stripslashes_deep( get_option( $this->cls )['rakuten']['html'] ); ?></textarea>
+					<textarea name="<?php echo $this->cls; ?>[rakuten][html]" rows="5" style="overflow-x: hidden;"><?php echo stripslashes_deep( get_option( $this->cls )['rakuten']['html'] ?? '' ); ?></textarea>
 				</label>
 			</p>
 			<?php for ( $i = 0;$i < 5;$i++ ) : ?>
 			<p class="textarea-wrap">
 				<label>
 					項目選択肢（改行区切）※選択肢は最大16文字：
-					<textarea name="<?php echo $this->cls; ?>[rakuten][select][]" rows="5" style="overflow-x: hidden;"><?php echo get_option( $this->cls )['rakuten']['select'][ $i ]; ?></textarea>
+					<textarea name="<?php echo $this->cls; ?>[rakuten][select][]" rows="5" style="overflow-x: hidden;"><?php echo get_option( $this->cls )['rakuten']['select'][ $i ] ?? ''; ?></textarea>
 				</label>
 			</p>
 			<?php endfor; ?>
