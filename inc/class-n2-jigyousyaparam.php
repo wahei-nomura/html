f74be5f30deafbefd7251605e10d4594c6a167eb
Taiki-nk <taikifate.0206@gmail.com>
2022-07-19 10:13:07 +0900

    :rocket:[update] 食品取扱意外の項目の追加も行うためファイル名やクラス名など変更
    foodをjigyousyaに変更した

--- inc/class-n2-jigyousyaparam.php
+++ inc/class-n2-jigyousyaparam.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * class-n2-foodparam.php
+ * class-n2-jigyousyaparam.php
  *
  * @package neoneng
  */
@@ -9,15 +9,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( class_exists( 'N2_Foodparam' ) ) {
-	new N2_Foodparam();
+if ( class_exists( 'N2_Jigyousyaparam' ) ) {
+	new N2_Jigyousyaparam();
 	return;
 }
 
 /**
  * Foodparam
  */
-class N2_Foodparam {
+class N2_Jigyousyaparam {
 	/**
 	 * クラス名
 	 *
@@ -29,7 +29,7 @@ class N2_Foodparam {
 	 */
 	public function __construct() {
 		$this->cls = get_class( $this );
-		add_action( 'admin_footer', array( $this, 'show_food_modal' ) );
+		add_action( 'admin_footer', array( $this, 'show_login_modal' ) );
 		add_action( 'admin_menu', array( $this, 'add_setup_menu' ) );
 		add_action( "wp_ajax_{$this->cls}", array( &$this, 'update_setupmenu' ) );
 	}
@@ -38,7 +38,7 @@ class N2_Foodparam {
 	 *
 	 * @return void
 	 */
-	public function show_food_modal() {
+	public function show_login_modal() {
 		$user = wp_get_current_user();
 		if ( 'jigyousya' !== $user->roles[0] ) {
 			return;
@@ -70,7 +70,7 @@ class N2_Foodparam {
 	 * クルー用セットアップ管理ページを追加
 	 */
 	public function add_setup_menu() {
-		add_menu_page( '食品取扱設定', '食品取扱設定', 'jigyousya', 'n2_food_menu', array( $this, 'add_jigyousya_setup_menu_page' ), 'dashicons-list-view' );
+		add_menu_page( '返礼品の設定', '返礼品の設定', 'jigyousya', 'n2_jigyousya_menu', array( $this, 'add_jigyousya_setup_menu_page' ), 'dashicons-list-view' );
 	}
 
 	/**
