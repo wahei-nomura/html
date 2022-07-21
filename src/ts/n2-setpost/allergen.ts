116d75b7a6d9fbaae47b0f551d0b102c0ea260ad
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-07-21 16:47:14 +0900

    :poop:[save] 別のバグを見つけたので一旦コミット
    issue114を改善してから再開

--- src/ts/n2-setpost/allergen.ts
+++ src/ts/n2-setpost/allergen.ts
@@ -7,6 +7,48 @@ export default () => {
 	 * 
 	================================================================== */
 	jQuery(function ($) {
-		console.log("allergen.ts読み込み");
+		class ControlAllergen {
+			public food: boolean;
+			public allergen: boolean;
+
+			constructor(food: boolean = false, allergen: boolean = false) {
+				this.food = food;
+				this.allergen = allergen;
+			}
+
+			get foodBool() {
+				return this.food;
+			}
+
+			set foodBool(food: boolean) {
+				this.food = food;
+			}
+
+			get allergenBool() {
+				return this.allergen;
+			}
+
+			set allergenBool(allergen: boolean) {
+				this.allergen = allergen;
+			}
+
+		}
+
+		const allergenState=new ControlAllergen();
+
+		console.log($('input[name="食品確認[]"]:checked').val())
+
+		const allergenBoolContent = $("label[for='アレルギー有無確認']")
+			.parent()
+			.parent();
+		const allergenListContent = $("label[for='アレルゲン']").parent().parent();
+		const tokkiContent = $("#アレルギーの特記事項").parent().parent();
+		allergenBoolContent.css("background-color", "pink");
+		allergenListContent.css("background-color", "yellow");
+		tokkiContent.css("background-color", "skyblue");
+
+		// allergenBoolContent.css("display", "none");
+		// allergenListContent.css("display", "none");
+		// tokkiContent.css("display", "none");
 	});
 };
