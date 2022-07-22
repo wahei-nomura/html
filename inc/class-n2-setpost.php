69806ef9192ad4b90292a3b330b2c93fbb1d12b5
Taiki-nk <taikifate.0206@gmail.com>
2022-07-22 10:32:39 +0900

    :+1:[fix] PHPでの削除ボタン出力取り消し

--- inc/class-n2-setpost.php
+++ inc/class-n2-setpost.php
@@ -177,9 +177,9 @@ class N2_Setpost {
 			'checkbox'         => '<li><label><input type=checkbox name="%1$s" value="%2$s" %3$s class="n2-input">%4$s</label></li>',
 			'select'           => '<select id="%1$s" name="%1$s" class="n2-input %3$s">%2$s</select>',
 			'option'           => '<option value="%1$s" %3$s>%2$s</option>',
-			'image'            => '<input class="n2-input %1$s-image-input" type="hidden" name="%2$s" value="%3$s"><button class="button button-primary %1$s-media-toggle">画像選択</button><button class="button button-secondary %1$s-media-delete">削除</button>
+			'image'            => '<input class="n2-input %1$s-image-input" type="hidden" name="%2$s" value="%3$s"><button class="button button-primary %1$s-media-toggle">画像選択</button>
 							<div><img class="%1$s-image-url" src="%3$s" alt="" width="50%%" /></div>',
-			'zip'              => '<input class="n2-input %1$s-zip-input" type="hidden" name="%2$s" value="%3$s"><button type="button" class="button button-primary %1$s-zip-toggle">zip選択</button><button type="button" class="button button-secondary %1$s-zip-delete">削除</button>
+			'zip'              => '<input class="n2-input %1$s-zip-input" type="hidden" name="%2$s" value="%3$s"><button type="button" class="button button-primary %1$s-zip-toggle">zip選択</button>
 							<div><p class="%1$s-zip-url">%4$s</p></div>',
 			'rakuten_genreid'  => '<button type="button" id="neo-neng-genreid-btn" class="button button-primary button-large">ディレクトリID検索</button><input type="hidden" id="%1$s" name="%1$s" value="%2$s"><input type="hidden" id="%3$s" name="%3$s" value="%4$s" class="%5$s">',
 			'rakuten_tagid'    => '<button type="button" id="neo-neng-tagid-btn" class="button button-primary button-large">タグID検索</button><input type="hidden" id="%1$s" name="%1$s" value="%2$s"><input type="hidden" id="%3$s" name="%3$s" value="%4$s" class="%5$s">',
