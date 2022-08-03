5fdfaf3ffc443f67fbceb23e98f95b44de408353
ryota <rnakanor@gmail.com>
2022-08-03 12:00:03 +0900

    :zap: とりあえず合計を取得してみた

--- inc/class-n2-sync.php
+++ inc/class-n2-sync.php
@@ -31,8 +31,16 @@ class N2_Sync {
 	 * @return void
 	 */
 	public function sync() {
-		$data = file_get_contents( 'https://steamship.co.jp/shiroishi/wp-json/wp/v2/posts?sku=test002' );
+		$data = file_get_contents( 'https://steamship.co.jp/kawatana/wp-json/wp/v2/posts?per_page=100' );
+		$headers = iconv_mime_decode_headers( implode( "\n", $http_response_header ) );
+		// 合計情報
+		$total = $headers['X-WP-Total'];
+		$pages = $headers['X-WP-TotalPages'];
+		echo '<pre>';
+		echo "合計：{$total}\n";
+		echo "ページ数：{$pages}\n";
 		$arr = json_decode( $data, true );
+		// exit;
 		foreach ( $arr as $v ) {
 
 			$postarr = array(
@@ -42,7 +50,8 @@ class N2_Sync {
 				'post_author' => $v['author'],
 				'meta_input'  => $v['acf'],
 			);
-			wp_insert_post( $postarr );
+			echo '<pre>';print_r($postarr);echo '</pre>';
+			// wp_insert_post( $postarr );
 		}
 		exit;
 	}
