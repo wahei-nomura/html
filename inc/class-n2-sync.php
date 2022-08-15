adb9af7b64c5dc4b5626d08cd1c10e3a5c1caf61
ryota <rnakanor@gmail.com>
2022-08-15 12:26:47 +0900

    :zap: 自治体の同期処理ajax追加（直列なので処理は遅い

--- inc/class-n2-sync.php
+++ inc/class-n2-sync.php
@@ -34,33 +34,47 @@ class N2_Sync {
 		$town = 'kawatana';
 		$url  = "https://steamship.co.jp/{$town}/wp-json/wp/v2/posts";
 		// params
-		$params  = http_build_query(
-			array(
-				'per_page' => 100,
-				'page'     => 5,
-			)
+		$params = array(
+			'per_page' => 100,
+			'page'     => 1,
 		);
-		$data    = file_get_contents( "{$url}?{$params}" );
-		$headers = iconv_mime_decode_headers( implode( "\n", $http_response_header ) );
-		// 合計情報
-		$total = $headers['X-WP-Total'];
-		$pages = $headers['X-WP-TotalPages'];
-		echo '<pre>';
-		echo "合計：{$total}\n";
-		echo "ページ数：{$pages}\n";
-		$arr = json_decode( $data, true );
-		// exit;
-		foreach ( $arr as $v ) {
-
-			$postarr = array(
-				'status'      => $v['publish'],
-				'type'        => $v['type'],
-				'post_title'  => $v['title']['rendered'],
-				'post_author' => $v['author'],
-				'meta_input'  => $v['acf'],
-			);
-			print_r($postarr);
-			// wp_insert_post( $postarr );
+		// ページ
+		$pages = 1;
+		while ( $params['page'] <= $pages ) {
+			$data    = file_get_contents( "{$url}?" . http_build_query( $params ) );
+			$headers = iconv_mime_decode_headers( implode( "\n", $http_response_header ) );
+			// 合計情報
+			$total = $headers['X-WP-Total'];
+			$pages = $headers['X-WP-TotalPages'];
+			$params['page']++;
+			$arr = json_decode( $data, true );
+			// exit;
+			foreach ( $arr as $v ) {
+				// 返礼品情報を生成
+				$postarr = array(
+					'status'      => $v['status'],
+					'type'        => $v['type'],
+					'post_title'  => $v['title']['rendered'],
+					'post_author' => $v['author'],
+					'meta_input'  => $v['acf'],
+				);
+				// 「返礼品コード」が既に登録済みか調査
+				$args = array(
+					'post_type'   => 'post',
+					'meta_key'    => '返礼品コード',
+					'meta_value'  => $v['acf']['返礼品コード'],
+					'post_status' => 'any',
+				);
+				// 返礼品の投稿IDを取得
+				$id = get_posts( $args )[0]->ID;
+				// 登録済みの場合
+				if ( $id ) {
+					$postarr['ID'] = $id;
+					wp_update_post( $postarr );
+					continue;
+				}
+				wp_insert_post( $postarr );
+			}
 		}
 		exit;
 	}
