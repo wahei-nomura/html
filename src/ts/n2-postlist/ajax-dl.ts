9906287c1248cc5e39f78d531caf2fd90c7b0464
山﨑洋義 <hiroyoshiyamasaki@yamasakimac.local>
2022-09-28 11:45:58 +0900

    画像DLクリック後の追加クラスをオーバーレイ要素自身に付けるよう変更

--- src/ts/n2-postlist/ajax-dl.ts
+++ src/ts/n2-postlist/ajax-dl.ts
@@ -6,7 +6,7 @@ export default () => {
 		// loading要素を追加
 		$('#download_img').after('<span class="loading_background"><span id="text_loading"></span><span class="progressbar"></span></span>');
 		$(document).on("click", '.dlbtn', (e) => {
-			$(e.target).addClass("not-click"); // クリックと同時にclass付けて二重クリックできないようにする
+			$('.loading_background').addClass("active"); // クリックと同時にオーバーレイ要素(loading_background)class付けて二重クリックできないようにする
 			text_loading.textContent = "登録画像確認中… "; // #text_loadingのテキスト書き換え(追加)
 			const btnName = $(e.target).attr("id");
 			e.preventDefault();
@@ -65,7 +65,7 @@ export default () => {
 			});
 			xhr.onreadystatechange = ()=>{
 				if (xhr.readyState === 4 && xhr.status === 200){
-					$('.not-click').removeClass("not-click"); 
+					$('.loading_background').removeClass("active"); 
 					text_loading.textContent = "";
 					$('.progressbar').css('width', dlper);
 				}
