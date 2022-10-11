49258d1dfbbbe86c3d67f88018298d83c8e819cc
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-10-11 10:52:16 +0900

    :shirt:[fix] オートフォーマット

--- src/ts/n2-postlist/copypost.ts
+++ src/ts/n2-postlist/copypost.ts
@@ -6,7 +6,7 @@ export default () => {
 	 * 投稿複製用
 	 * 
 	================================================================== */
-	jQuery(function($) {
+	jQuery(function ($) {
 
 		type setData = {
 			title: string,
@@ -16,7 +16,7 @@ export default () => {
 			everyDate: number | null,
 		}
 
-		const createCopyPost=(id: number, setData: setData): void => {
+		const createCopyPost = (id: number, setData: setData): void => {
 			$.ajax({
 				type: "POST",
 				url: ajaxUrl(window),
@@ -27,7 +27,7 @@ export default () => {
 				},
 			})
 				.done((res) => {
-					const response=JSON.parse(res)
+					const response = JSON.parse(res)
 					alert(`「${response['title']}」を作成しました`);
 					console.log(response)
 					location.reload();
@@ -39,19 +39,19 @@ export default () => {
 		};
 
 		$(`.${prefix}-copypost-btn`).on("click", (e) => {
-			const itemTr=$(e.target).parent().parent();
-			const originalId: number=Number(
+			const itemTr = $(e.target).parent().parent();
+			const originalId: number = Number(
 				itemTr.find("th.check-column input").val()
 			);
-			const itemTitle=itemTr.find(".item-title a").text();
+			const itemTitle = itemTr.find(".item-title a").text();
 			openModal(originalId, itemTitle);
 		});
 
-		const openModal=(id:number, title:string):void => {
+		const openModal = (id: number, title: string): void => {
 			// テンプレートディレクトリからHTMLをロード
 			$("#wpbody-content").append(`<div id="${prefix}-content"></div>`);
 			$(`#${prefix}-content`).load(
-				neoNengPath(window)+"/template/copy-post.html",
+				neoNengPath(window) + "/template/copy-post.html",
 				() => {
 					$("#n2-copypost-modal .original-title").text(title);
 					$('input[name="複写後商品名"]').val(title);
@@ -59,12 +59,12 @@ export default () => {
 				}
 			);
 		};
-		
-		$('body').on('change', 'select[name="定期"]', e => {
-			const teikiNum=$(e.target).val();
-			$('#n2-copypost-modal .new-title span').text(teikiNum? `【全${teikiNum}回定期便】`:'');
 
-			if(teikiNum) {
+		$('body').on('change', 'select[name="定期"]', e => {
+			const teikiNum = $(e.target).val();
+			$('#n2-copypost-modal .new-title span').text(teikiNum ? `【全${teikiNum}回定期便】` : '');
+
+			if (teikiNum) {
 				$('.is-teiki').css('display', 'block')
 			} else {
 				$('.is-teiki').css('display', 'none')
@@ -80,15 +80,15 @@ export default () => {
 
 		$("body").on("click", "#n2-copypost-modal .submit", () => {
 
-			if(
-				$('select[name="定期"] option:selected').val()>1&&
-				($('input[name="同月回数"]').val()===''||$('input[name="初回発送日"]').val()===''||$('input[name="毎月発送日"]').val()==='')
+			if (
+				$('select[name="定期"] option:selected').val() > 1 &&
+				($('input[name="同月回数"]').val() === '' || $('input[name="初回発送日"]').val() === '' || $('input[name="毎月発送日"]').val() === '')
 			) {
 				alert('全ての項目を入力してください')
 				return
 			}
 
-			const setData:setData={
+			const setData: setData = {
 				title: String($('input[name="複写後商品名"]').val()),
 				teiki: Number($('select[name="定期"] option:selected').val()),
 				monthlyNumber: Number($('input[name="同月回数"]').val()),
@@ -96,7 +96,7 @@ export default () => {
 				everyDate: Number($('input[name="毎月発送日"]').val())
 			}
 
-			createCopyPost(Number($("#n2-copypost-modal input[name='id']").val()),setData);
+			createCopyPost(Number($("#n2-copypost-modal input[name='id']").val()), setData);
 		});
 	});
 };
