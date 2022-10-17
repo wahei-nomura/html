342404228d178e54dadc46dff427b4a6bad691a6
hirotoki3193 <1stlevi777@gmail.com>
2022-10-17 16:26:22 +0900

    :bug:[fix] itemde-tail.tsを商品詳細ページでのみ動作するように変更
    ・itemde-tail.tsの動作設定変更
    ・旧ini読み込んでるのでコメントアウト

--- single.php
+++ single.php
@@ -7,11 +7,11 @@
 
 global $post;
 $post_data = N2_Functions::get_all_meta( $post );
-echo esc_html(get_post_type_object(get_post_type())->name);
-$ini = parse_ini_file( get_theme_file_path() . '/config/n2-fields.ini', true );
+// echo esc_html(get_post_type_object(get_post_type())->name);
+// $ini = parse_ini_file( get_theme_file_path() . '/config/n2-fields.ini', true );
 
-// プラグインn2-developのn2_setpost_show_customfields呼び出し
-$fields = apply_filters( 'n2_setpost_show_customfields', $ini, 'default' );
+// // プラグインn2-developのn2_setpost_show_customfields呼び出し
+// $fields = apply_filters( 'n2_setpost_show_customfields', $ini, 'default' );
 
 ?>
 
@@ -44,5 +44,5 @@ $fields = apply_filters( 'n2_setpost_show_customfields', $ini, 'default' );
 <body <?php body_class(); ?>>
 
 
-
+<?php
 get_template_part('template/item_details');
