ea53f9cc3a042b6b0b83b45f86574f6a05b7dc43
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-10-17 14:50:07 +0900

    :rocket:[fix] 事業者の管理画面回遊制限追加

--- inc/class-n2-setmenu.php
+++ inc/class-n2-setmenu.php
@@ -24,6 +24,8 @@ class N2_Setmenu {
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'change_menulabel' ) );
 		add_action( 'admin_menu', array( $this, 'remove_menulabel' ) );
+		add_action( 'init', array( $this, 'not_edit_user' ) );
+
 	}
 
 	/**
@@ -41,18 +43,45 @@ class N2_Setmenu {
 	 * remove_menulabel
 	 */
 	public function remove_menulabel() {
+		// クルー事業者共通で削除
 		$menus = array(
 			'edit.php?post_type=page', // 固定ページ
 			'edit-comments.php', // コメント
+			'tools.php',
+			'upload.php',
+			'profile.php',
 		);
 
-		// ss-crewは外見メニュー削除
+		// クルーのメニュー削除
 		if ( 'ss-crew' === wp_get_current_user()->roles[0] ) {
 			array_push( $menus, 'themes.php' );
+			array_push( $menus, 'upload.php' );
+		}
+
+		// 事業者のメニュー削除
+		if ( 'jigyousya' === wp_get_current_user()->roles[0] ) {
+			array_push( $menus, 'index.php' );
 		}
 
 		foreach ( $menus as $menu ) {
 			remove_menu_page( $menu );
+		}
+	}
+
+
+	/**
+	 * not_edhit_user
+	 */
+	public function not_edit_user() {
+		global $pagenow;
+		if ( ! current_user_can( 'jigyousya' ) ) {
+			return;
+		}
+		if ( 'index.php' === $pagenow || 'tools.php' === $pagenow || 'upload.php' === $pagenow ) {
+			wp_redirect( admin_url( 'edit.php' ) );
+		}
+		if ( 'profile.php' === $pagenow ) {
+			wp_die( 'ユーザープロフィールを変更したい場合は「Steamship」へお問い合わせください。<p><a class="button" href="' . $_SERVER['HTTP_REFERER'] . '">前に戻る</a></p>' );
 		}
 	}
 }
