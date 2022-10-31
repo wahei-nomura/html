5c2792eeaaf319aac6ce3dfb8c5bb0ddea88216a
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-10-31 13:42:43 +0900

    :tada:[feat] とりあえずクルー用確認ページ完成

--- template/crew-check.php
+++ template/crew-check.php
@@ -15,8 +15,14 @@
 			width: 100%;
 			border-spacing: 0;
 			
-			tr:nth-child(even) {
+			tr {
 				background: #eee;
+				&.aleart {
+					background-color: pink;
+				}
+				&.normal.none {
+					display: none;
+				}
 			}
 						
 			th{
@@ -28,20 +34,28 @@
 				border-bottom: solid 2px #ddd;
 				text-align: center;
 				padding: 4px 16px;
-				max-width: 600px; /* 省略せずに表示するサイズを指定 */
+				max-width: 500px; /* 省略せずに表示するサイズを指定 */
 				white-space: nowrap;
 				overflow: hidden;
 				text-overflow: ellipsis;
 			}
 		}
+		.change-btn {
+			cursor: pointer;
+			margin-bottom: 8px;
+			padding: 4px 8px;
+		}
 	}
 </style>
 
 <section class="n2-crew-check">
+	<h1>事業者確認状況（クルー専用ページ）</h1>
+	<button class="change-btn">確認済みを非表示</button>
 	<table>
 	<tbody>
 		<tr>
 			<th>事業者名</th>
+			<th>コード</th>
 			<th>商品名</th>
 			<th>確認状況</th>
 			<th>公開日</th>
@@ -50,12 +64,14 @@
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
+			$item_checked = ! empty( get_post_meta( get_the_ID(), '事業者確認', true ) ) ? true : false;
 			?>
-			<tr>
+			<tr class="<?php echo ! $item_checked ? 'aleart' : 'normal'; ?>">
 				<td><?php the_author(); ?></td>
+				<td><?php echo get_post_meta( get_the_ID(), '返礼品コード', true ); ?></td>
 				<td><a href="<?php the_permalink(); ?>" target=”_blank”><?php the_title(); ?></a></td>
-				<td>未</td>
-				<td>2022-10-31</td>
+				<td><?php echo $item_checked ? '済' : '未'; ?></td>
+				<td><?php the_modified_date(); ?></td>
 			</tr>
 			<?php
 		endwhile;
@@ -64,3 +80,11 @@
 </tbody>
 </table>
 </sectoin>
+
+<script>
+	jQuery(function($){
+		$('.n2-crew-check .change-btn').on('click', ()=>{
+			$('.n2-crew-check .normal').toggle('none')
+		})
+	})
+</script>
