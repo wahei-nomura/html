d8bb49ae962b6480fb74520a8e9836f9c539de22
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-02 15:21:26 +0900

    :bug:[fix] 一応かっこ仮にしとこ

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>NEO NENG【<?php bloginfo( 'name' ); ?>】</title>
+	<title>NEO NENG(仮)【<?php bloginfo( 'name' ); ?>】</title>
 
 	<meta name="viewport" content="width=device-width">
 	<!-- google Material -->
@@ -18,5 +18,5 @@
 <?php wp_body_open(); ?>
 
 <header class="p-3 bg-dark text-white text-center">
-	<h1>NEO NENG【<?php bloginfo( 'name' ); ?>】</h1>
+	<h1>NEO NENG(仮)【<?php bloginfo( 'name' ); ?>】</h1>
 </header>
