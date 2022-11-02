e722aafa067bf1731ac650d389ec6b9e8b285398
hirotoki3193 <1stlevi777@gmail.com>
2022-11-02 14:55:51 +0900

    :sparkles:[add] Bootstrap5追加
    ・header.phpにCDN追加

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
 <meta name="viewport" content="width=device-width">
+<!-- google Material -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+<!-- Bootstrap5 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <?php wp_head(); ?>
