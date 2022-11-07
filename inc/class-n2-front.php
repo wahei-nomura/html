b403938e4ae0ad3a390c1f3aa94053b93a84352b
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-07 15:42:12 +0900

    :sparkles:[fix] タイムスタンプ保存

--- inc/class-n2-front.php
+++ inc/class-n2-front.php
@@ -208,9 +208,10 @@ class N2_Front {
 	 * ajaxで事業者確認パラメーターを更新
 	 */
 	public function update_item_confirm() {
+		date_default_timezone_set( 'Asia/Tokyo' );
 		$post_id      = filter_input( INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT );
 		$confirm_flag = filter_input( INPUT_POST, 'confirm_flag', FILTER_VALIDATE_BOOLEAN ) ? '確認未' : '確認済み';
-		update_post_meta( $post_id, '事業者確認', array( $confirm_flag ) );
+		update_post_meta( $post_id, '事業者確認', array( $confirm_flag, date( 'Y-m-d G:i:s' ) ) );
 	}
 
 	/**
