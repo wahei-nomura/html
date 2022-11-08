f01939a94ff7b9b001fbcf6efea7e932fd0d2aaf
hirotoki3193 <1stlevi777@gmail.com>
2022-11-08 16:37:36 +0900

    :bug:[fix] ポータル田代関連の変更
    ・hook名変更
    ・旧ajax削除など

--- template/product-page/product-imgs.php
+++ template/product-page/product-imgs.php
@@ -1,14 +1,35 @@
+<?php if ( isset( $args ) && $args ) : ?>
 <section class="product-imgs">
-    <div class="img-box">
-        <img class='main-img' src="<?php echo $args[0]; ?>" width='100%'>
-    </div>
-    <div class="wrapper-img-list">
-        <ul class='sub-imgs'>
-            <?php for ( $i = 0; $i < 2; $i++ ) : ?>
-            <?php foreach ( $args as $img_url ) : ?>
-                <img class='sub-img' src="<?php echo $img_url; ?>" width='100%' height="100%">
-            <?php endforeach; ?>
-            <?php endfor; ?>
-        </ul>
-    </div>
+	<div class="img-box">
+		<img class='main-img' src="<?php echo $args[0]; ?>" width='100%'>
+	</div>
+	<div class="wrapper-img-list">
+		<ul class='sub-imgs'>
+		<?php for ( $i = 0; $i < 2; $i++ ) : ?>
+			<?php foreach ( $args as $img_url ) : ?>
+				<img class='sub-img' src="<?php echo $img_url; ?>" width='100%' height="100%">
+			<?php endforeach; ?>
+		<?php endfor; ?>
+		</ul>
+	</div>
 </section>
+<?php else : ?>
+<section class="product-imgs">
+	<div class="img-box">
+		<div class="product-img-wrap">
+			<span class="product-img-section">No Image</span>
+		</div>
+	</div>
+	<div class="wrapper-img-list">
+		<ul class='sub-imgs'>
+		<?php for ( $i = 0; $i < 10; $i++ ) : ?>
+			<?php foreach ( $args as $img_url ) : ?>
+				<div class="product-img-wrap">
+					<span class="product-img-section">No Image</span>
+				</div>
+			<?php endforeach; ?>
+		<?php endfor; ?>
+		</ul>
+	</div>
+</section>
+<?php endif; ?>
