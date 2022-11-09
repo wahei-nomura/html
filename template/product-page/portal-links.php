621a1ab9b41c5811fd6fba834a9251d36e539a09
hirotoki3193 <1stlevi777@gmail.com>
2022-11-09 10:12:06 +0900

    :bug:[fix] 田代分散に伴う変更等
    ・田代hook変更
    ・取得できなかったリンク表示変更
    etc...

--- template/product-page/portal-links.php
+++ template/product-page/portal-links.php
@@ -2,9 +2,15 @@
     <h2 class="">各ポータルサイト</h2>
     <ul class="link-list">
     <?php foreach ( $args as $portal => $param ) : ?>
+        <?php if ( $param['item_url'] ) : ?>
         <li class="link-btn">
             <a href="<?php echo $param['item_url']; ?>" target="_blank"><?php echo $portal; ?></a>
         </li>
+        <?php else : ?>
+            <li class="link-btn disable">
+                <span><?php echo $portal; ?></span>
+            </li>
+        <?php endif; ?>
     <?php endforeach; ?>
     </ul>
 </section>
