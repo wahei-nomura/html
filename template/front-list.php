9ca4a95d2ce8ff74f38b5cc7b66cf360fc454244
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-11 11:05:18 +0900

    :sparkles:[feat] ページネーション調整OK

--- template/front-list.php
+++ template/front-list.php
@@ -15,17 +15,8 @@
 		<p>お手数ですが、各商品をご確認されましたら<span class="text-danger">「確認OK」</span>ボタンを押してください。（ご不明点はスチームシップまでお問い合わせください。）</p>
 	<?php endif; ?>
 
-	<?php
-	the_posts_pagination(
-		array(
-			// 'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
-			'prev_next' => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
-			'prev_text' => __( '前へ' ), // 「前へ」リンクのテキスト
-			'next_text' => __( '次へ' ), // 「次へ」リンクのテキスト
-			'type'      => 'list', // 戻り値の指定 (plain/list)
-		)
-	);
-	?>
+	<?php get_template_part( 'template/pagination' ); ?>
+
 	<ul class="product-list">
 		<?php
 		if ( have_posts() ) {
@@ -102,17 +93,7 @@
 		?>
 
 	</ul>
-	<?php
-	the_posts_pagination(
-		array(
-			// 'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
-			'prev_next' => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
-			'prev_text' => __( '前へ' ), // 「前へ」リンクのテキスト
-			'next_text' => __( '次へ' ), // 「次へ」リンクのテキスト
-			'type'      => 'list', // 戻り値の指定 (plain/list)
-		)
-	);
-	?>
+	<?php get_template_part( 'template/pagination' ); ?>
 
 	<?php
 		if(!isset($_GET['jigyousya'])){ //事業者パラメーターが存在しない場合
