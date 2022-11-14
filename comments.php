c1e11aabcd196705dbe1287815abdca0d23a3aac
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-14 16:04:48 +0900

    :sparkles:[feat] コメントフォームをBootstrapでいい感じに

--- comments.php
+++ comments.php
@@ -12,7 +12,7 @@
 <script src="https://cdn.jsdelivr.net/gh/steamships/in-browser-sass/dist/in-browser-sass.bundle.min.js"></script>
 
 <style type="text/scss">
-	#comments {
+	/* #comments {
 		li {
 			list-style: none;
 		}
@@ -33,7 +33,6 @@
 		}
 
 		.comment-body {
-			/* background-color: white; */
 			border-bottom: solid 1px white;
 			padding: 8px 0;
 			.comment-author {
@@ -58,7 +57,7 @@
 		a.comment-reply-link {
 			display: none;
 		}
-	}
+	} */
 </style>
 
 <?php if ( comments_open() ) : ?>
@@ -76,25 +75,32 @@
 		$author = get_userdata( $post->post_author )->display_name;
 		?>
 
-		<div id="respond" class="comment-respond">
-			<h3 id="reply-title" class="comment-reply-title">返礼品に関する変更要望など <small><a rel="nofollow" id="cancel-comment-reply-link" href="/ojika/?p=3990&amp;look=true#respond" style="display:none;">コメントをキャンセル</a></small></h3>
-			<form action="<?php echo home_url( '/wp-comments-post.php' ); ?>" method="post" id="commentform" class="comment-form">
-				<p class="comment-form-comment">
-					<label>内容</label>
-					<textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea>
-				</p>		
-				<p class="comment-form-author">
-					<label for="author">送信者</label>
-					<select id="author" name="author">
-						<option value="<?php echo $author; ?>"><?php echo $author; ?></option>
-						<option value="スチームシップ" <?php echo $selected; ?>>スチームシップ</option>
-					</select>
-				</p>
-				<p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="コメントを送信">
-					<input type="hidden" name="comment_post_ID" value="<?php echo $post->ID; ?>" id="comment_post_ID">
-					<input type="hidden" name="comment_parent" id="comment_parent" value="0">
-				</p>
-			</form>
+		<div class='card shadow text-dark bg-light mb-3 p-4'>
+			<div class='card-body'>
+				<h3 class='card-title text-primary mb-5'>返礼品に関する変更要望など <small><a rel="nofollow" href="/ojika/?p=3990&amp;look=true#respond" style="display:none;">コメントをキャンセル</a></small></h3>
+				<form action="<?php echo home_url( '/wp-comments-post.php' ); ?>" method="post" id="commentform" class="comment-form">
+					<div class="row mb-3">
+						<label class='col-sm-2 col-form-label col-form-label-sm fs-5'>内容</label>
+						<div class='col-sm-10'>
+							<textarea id="comment" class='form-control' name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea>
+						</div>
+					</div>		
+					<div class="row mb-5">
+						<label for="author" class='col-sm-2 col-form-label col-form-label-sm fs-5'>送信者</label>
+						<div class='col-sm-6'>
+							<select id="author" name="author" class='form-control'>
+								<option value="<?php echo $author; ?>"><?php echo $author; ?></option>
+								<option value="スチームシップ" <?php echo $selected; ?>>スチームシップ</option>
+							</select>
+						</div>
+					</div>
+					<div class="row">
+						<input name="submit" type="submit" class="btn btn-outline-primary" value="コメントを送信">
+						<input type="hidden" name="comment_post_ID" value="<?php echo $post->ID; ?>" id="comment_post_ID">
+						<input type="hidden" name="comment_parent" id="comment_parent" value="0">
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 <?php endif; ?>
