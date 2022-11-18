b5d57a9722b6103aa524f10a6ab28e2ff3fa469b
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-18 15:19:13 +0900

    :+1:[feat] テーブル化

--- template/all-town.php
+++ template/all-town.php
@@ -4,13 +4,36 @@
  *
  * @package neoneng
  */
+?>
 
+<!-- Bootstrap5 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+<table class="table table-dark table-hover">
+  <thead>
+	<tr>
+		<th scope="col">自治体</th>
+		<th scope="col">事業者確認状況</th>
+		<th scope="col">コメント状況</th>
+	</tr>
+  </thead>
+  <tbody>
+  <?php
 $all_site = get_sites();
 	foreach ( $all_site as $site ) :
 		$site_details = get_blog_details( $site->blog_id );
 		if ( '1' !== $site->blog_id ) :
 ?>
-	<p><a href="<?php echo $site_details->siteurl; ?>" target="_blank"><?php echo $site_details->blogname; ?></a></p>
-<?php
+	<tr>
+		<td><a href="<?php echo $site_details->siteurl; ?>" target="_blank"><?php echo $site_details->blogname; ?></a></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<?php
 		endif;
 	endforeach;
+?>
+  </tbody>
+</table>
