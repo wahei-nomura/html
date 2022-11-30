4373dd1c0587659c9fe72b8837b573956245b8c2
Taiki-ss <taiki.ishida.ss@gmail.com>
2022-11-30 14:44:08 +0900

    :bug:[fix] 検索条件表示間違ってコメントアウトしちゃってた

--- template/front-list.php
+++ template/front-list.php
@@ -10,43 +10,43 @@
 
 <?php
 		// if( empty( $_GET['look']) && empty( $_GET['crew'])){
-		// 	$user_lists = get_users( 'role=jigyousya' );
-		// 	// var_dump($user_lists);
-		// 	$search_params = $_GET;
-		// 	$search_result = '';
-		// 	foreach ( $search_params as $key => $sch_prm ) {
-		// 		if('' != $sch_prm && 'paged' != $key){
-		// 			if ( '' != $search_result ) {
-		// 				$search_result .= ', ';
-		// 			}
-		// 			if( 'jigyousya' == $key){
-		// 				$keyNo = array_search($sch_prm, array_column($user_lists, 'ID'));
-		// 				$search_result .= $user_lists[$keyNo]->display_name;
-		// 			}elseif('返礼品コード' == $key){
-		// 				foreach( $sch_prm as $codeKey => $code_prm){
-		// 					$code_meta_data = get_post_meta($code_prm);
-		// 					$codes = $code_meta_data['返礼品コード'];
-		// 					foreach($codes as $cdKey => $cd){
-		// 						$search_result .= $cd;
-		// 					}
-		// 					if($codeKey != array_key_last($sch_prm)){
-		// 						$search_result .= '/';
-		// 					}
-		// 				}
-		// 			}elseif( 'sortcode' == $key){
-		// 				if( 'sortbycode' == $sch_prm){
-		// 					$search_result .= 'コード順に表示';
-		// 				}else{
-		// 					$search_result .= '登録順に表示';
-		// 				}
-		// 			}else{
-		// 				$search_result .= $sch_prm;
-		// 			}
-		// 		}
-		// 	}
-		// 	if ( '' != $search_result ) {
-		// 		echo '<h2 class="search-result-header text-primary">絞り込み：' . $search_result . '</h2>';
-		// 	}
+			$user_lists = get_users( 'role=jigyousya' );
+			// var_dump($user_lists);
+			$search_params = $_GET;
+			$search_result = '';
+			foreach ( $search_params as $key => $sch_prm ) {
+				if('' != $sch_prm && 'paged' != $key){
+					if ( '' != $search_result ) {
+						$search_result .= ', ';
+					}
+					if( 'jigyousya' == $key){
+						$keyNo = array_search($sch_prm, array_column($user_lists, 'ID'));
+						$search_result .= $user_lists[$keyNo]->display_name;
+					}elseif('返礼品コード' == $key){
+						foreach( $sch_prm as $codeKey => $code_prm){
+							$code_meta_data = get_post_meta($code_prm);
+							$codes = $code_meta_data['返礼品コード'];
+							foreach($codes as $cdKey => $cd){
+								$search_result .= $cd;
+							}
+							if($codeKey != array_key_last($sch_prm)){
+								$search_result .= '/';
+							}
+						}
+					}elseif( 'sortcode' == $key){
+						if( 'sortbycode' == $sch_prm){
+							$search_result .= 'コード順に表示';
+						}else{
+							$search_result .= '登録順に表示';
+						}
+					}else{
+						$search_result .= $sch_prm;
+					}
+				}
+			}
+			if ( '' != $search_result ) {
+				echo '<h2 class="search-result-header text-primary">絞り込み：' . $search_result . '</h2>';
+			}
 		// }
 	?>
 
