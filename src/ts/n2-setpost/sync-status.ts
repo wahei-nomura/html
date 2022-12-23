bf59cb089c5120293a49be6b53c9c5f6932bd705
Taiki-nk <taikifate.0206@gmail.com>
2022-12-23 17:55:41 +0900

    :rocket:[fix] プログレストラッカーのクラス絞り込みを強化

--- src/ts/n2-setpost/sync-status.ts
+++ src/ts/n2-setpost/sync-status.ts
@@ -11,13 +11,10 @@ export default () => {
 		wp(window).data.subscribe(() => {
 			// 投稿が更新されたかどうかを判定する
 			if (wp(window).data.select('core/editor').isSavingPost()) {
-				// 投稿が更新されている場合の処理をここに記述する
-				$(`#${prefix}-progress-tracker li`).removeClass('active')
-				$(
-					`.${wp(window)
-						.data.select("core/editor")
-						.getEditedPostAttribute("status")}`
-				).addClass("active");
+				// 投稿ステータスとプログレストラッカーそれぞれのclass名が連動しておりactiveを付け替えている
+				$(`#${prefix}-progress-tracker li`).removeClass('active').parent().find(`.${wp(window)
+					.data.select("core/editor")
+					.getEditedPostAttribute("status")}`).addClass("active");
 			}
 		});
 	})
