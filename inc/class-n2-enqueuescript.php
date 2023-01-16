5a7c2fd06fee08b0d69980f4bd13df29aede5349
ryota-nakano <rnakanor@gmail.com>
2023-01-16 11:11:51 +0000

    :poop: 一旦jsの読み込み止め

--- inc/class-n2-enqueuescript.php
+++ inc/class-n2-enqueuescript.php
@@ -31,10 +31,10 @@ class N2_Enqueuescript {
 	 *
 	 * @return void
 	 */
-	public function enqueue_setpost_script() {
+	public function enqueue_setpost_script( $hook_suffix ) {
 		wp_enqueue_media();
-		wp_enqueue_script( 'n2-script', get_theme_file_uri( 'dist/admin.js' ), array( 'jquery' ), N2_CASH_BUSTER, true );
-		wp_enqueue_script( 'jquery-touch-punch', false, array( 'jquery' ), N2_CASH_BUSTER, true );
+		// wp_enqueue_script( 'n2-script', get_theme_file_uri( 'dist/admin.js' ), array( 'jquery' ), N2_CASH_BUSTER, true );
+		// wp_enqueue_script( 'jquery-touch-punch', false, array( 'jquery' ), N2_CASH_BUSTER, true );
 		wp_enqueue_style( 'n2-style', get_theme_file_uri( 'dist/admin.css' ), array(), N2_CASH_BUSTER );
 
 		wp_localize_script( 'n2-script', 'tmp_path', $this->get_tmp_path() );
