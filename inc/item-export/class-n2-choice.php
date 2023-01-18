c5330c5deb51ea62b70b5c6aba5b4517cb5edf70
Junki-Kawatani <junki.kawatani@steamship.co.jp>
2023-01-18 16:24:13 +0900

    :green-heart:[fix] TSV不備修正
    TSVの中身が入っていない物を修正

--- inc/item-export/class-n2-choice.php
+++ inc/item-export/class-n2-choice.php
@@ -23,10 +23,11 @@ class N2_Choice {
 	 * @return void
 	 */
     public function create_tsv() {
-        $header_data = yaml_parse_file( get_theme_file_path() . '/config/n2-file-header.yml' );
+        $header_data = yaml_parse_file( get_theme_file_path( '/config/n2-file-header.yml' ) );
         $items_arr = array();
         $check_arr = array();
         $delimiter = '" "'; //tsvデータ
+        $opt = get_option( 'N2_Setupmenu' );
 
         // あとでヘッダの上の連結するのに必要
 		$tsv_title = $header_data[ 'choice' ][ 'tsv_header' ][ 'title' ];
@@ -42,12 +43,11 @@ class N2_Choice {
         // ajaxで渡ってきたpostidの配列
 		$ids = explode( ',', filter_input( INPUT_POST, 'choice' ) );
 
-        $sumple_header = trim(file_get_contents(str_replace("//", "//{$auth[ 'user' ]}:{$auth[ 'pass' ]}@", "{$auth[ 'url' ]}")));
-        $sumple_header = array_flip(explode("\t", $sumple_header));
+        $sumple_header = trim( file_get_contents( str_replace( "//", "//{$auth[ 'user' ]}:{$auth[ 'pass' ]}@", "{$auth[ 'url' ]}" ) ) );
+        $sumple_header = array_flip( explode( "\t", $sumple_header ) );
 
         foreach( $ids as $id ){
-            $items_arr[ $id ] = array(...$sumple_header, ...get_post_meta( $id, '', false ));
-
+            $items_arr[ $id ] = array(...$sumple_header, ...get_post_meta( $id, '', false ) );
             foreach ( $items_arr[ $id ] as $k => $v ) {
                 #初期化
                 if ( in_array( $k, $header0 ) ) {
@@ -63,19 +63,85 @@ class N2_Choice {
 
             $arr = array(
                 '管理コード'      => strtoupper( get_post_meta( $id, "返礼品コード", true ) ),
-                '（必須）お礼の品名'       => N2_Functions::special_str_convert( get_the_title( $id ) ) . " [{$arr[$id]['管理コード']}]",
+                '（必須）お礼の品名'       => N2_Functions::special_str_convert( get_the_title( $id ) ) . " [{$items_arr[$id]['管理コード']}]",
                 // サイト表示事業者名：ポータル表示用名称が登録されてたら優先。「記載しない」になってたら空欄で出力。
-                'サイト表示事業者名'      => get_the_author_meta( "portal", get_post_field( "post_author", $id ) ) ? ( get_the_author_meta( "portal", get_post_field( "post_author", $id ) ) === '記載しない' ? '' : get_the_author_meta("portal", get_post_field( "post_author", $id ) ) ) : get_the_author_meta( "first_name", get_post_field( "post_author", $id ) ), 
-                'サイト表示事業者名' => get_post_meta($id, "提供事業者名", true) ?: $arr[$id]['サイト表示事業者名'], // NENGに提供事業者名が入ってたら優先
-                '（条件付き必須）必要寄付金額'      => get_post_meta($id, "寄附金額", true),
-                'キャッチコピー'  => N2_Functions::special_str_convert(get_post_meta($id, "キャッチコピー１", true)),
-                '説明'     => N2_Functions::special_str_convert(get_post_meta($id, "説明文", true)) . (get_post_meta($id, "検索キーワード", true) ? "\n\n" . (N2_Functions::special_str_convert(get_post_meta($id, "検索キーワード", true))) : "") . (preg_match('/hasami/', esc_url($_SERVER['REQUEST_URI'])) ? (get_post_meta($id, "楽天カテゴリー", true) ? "\n\n" . (N2_Functions::special_str_convert(get_post_meta($id, "楽天カテゴリー", true))) : '') : ''),
-                '容量'    => N2_Functions::special_str_convert(get_post_meta($id, "内容量・規格等", true)) . ((preg_match('/hasami|toki|ureshino|tajimi/', esc_url($_SERVER['REQUEST_URI'])) ? ((get_post_meta($id, "電子レンジ対応", true) != "-" || get_post_meta($id, "オーブン対応", true) != "-" || get_post_meta($id, "食洗機対応", true) != "-") ? "\n\n【対応機器】\n電子レンジ" . get_post_meta($id, "電子レンジ対応", true) . " / オーブン" . get_post_meta($id, "オーブン対応", true) . " / 食器洗浄機" . get_post_meta($id, "食洗機対応", true) . ((get_post_meta($id, "対応機器備考", true) != "") ? "\n※" . get_post_meta($id, "対応機器備考", true) : "") : "") : "") . ((preg_match('/hasami|toki|tajimi/', esc_url($_SERVER['REQUEST_URI'])) && get_post_meta($id, "やきもの", true) !== "やきものではない") ? "\n\n【お申し込み前に必ずお読みください】\nお届けする返礼品には個体差が生じる場合があります。\n下部記載の「個体差について」、「破損・欠陥について」を必ずご一読のうえ、お申し込みください。" . (preg_match('/toki|tajimi/', esc_url($_SERVER['REQUEST_URI'])) ? "\n\n【色について】\n商品写真はできる限り実物の色に近づけるよう徹底しておりますが、 お使いのモニター設定、お部屋の照明等により実際の商品と色味が異なる場合がございます。" : "") : "")) . (get_post_meta($id, "原料原産地", true) ? "\n\n【原料原産地】\n" . N2_Functions::special_str_convert(get_post_meta($id, "原料原産地", true)) : "") . (get_post_meta($id, "加工地", true) ? "\n\n【加工地】\n" . N2_Functions::special_str_convert(get_post_meta($id, "加工地", true)) : ""),
+                'サイト表示事業者名'      => get_post_meta( $id, "提供事業者名", true ) 
+                                                ?: 
+                                                    (
+                                                        get_the_author_meta( "portal", get_post_field( "post_author", $id ) ) 
+                                                        ? (
+                                                            get_the_author_meta( "portal", get_post_field( "post_author", $id ) ) === '記載しない' 
+                                                                ? '' 
+                                                                : get_the_author_meta("portal", get_post_field( "post_author", $id ) ) 
+                                                            ) 
+                                                        : get_the_author_meta( "first_name", get_post_field( "post_author", $id ) )
+                                                    ),
+                '（条件付き必須）必要寄付金額'      => get_post_meta( $id, "寄附金額", true ),
+                'キャッチコピー'  => N2_Functions::special_str_convert( get_post_meta( $id, "キャッチコピー１", true ) ),
+                '説明'     => N2_Functions::special_str_convert( get_post_meta( $id, "説明文", true ) ) . 
+                                (
+                                    get_post_meta( $id, "検索キーワード", true )  
+                                        ? "\n\n" . ( N2_Functions::special_str_convert( get_post_meta( $id, "検索キーワード", true ) ) ) 
+                                        : ""
+                                ) . 
+                                (
+                                    preg_match( '/hasami/', esc_url( $_SERVER[ 'REQUEST_URI' ] ) ) 
+                                        ? ( get_post_meta($id, "楽天カテゴリー", true )  
+                                            ? "\n\n" . ( N2_Functions::special_str_convert( get_post_meta( $id, "楽天カテゴリー", true ) ) ) 
+                                            : '' ) 
+                                        : ''
+                                ) . "\n\n" . $opt['add_text'][get_bloginfo('name')],
+                '容量'    => N2_Functions::special_str_convert( get_post_meta( $id, "内容量・規格等", true ) ) . 
+                                (
+                                    (
+                                        preg_match( '/hasami|toki|ureshino|tajimi/', esc_url( $_SERVER[ 'REQUEST_URI' ] ) ) 
+                                            ? (
+                                                ( get_post_meta($id, "電子レンジ対応", true ) != "-" || get_post_meta( $id, "オーブン対応", true ) != "-" || get_post_meta( $id, "食洗機対応", true ) != "-" ) 
+                                                    ? "\n\n【対応機器】\n電子レンジ" . get_post_meta( $id, "電子レンジ対応", true ) . " / オーブン" . get_post_meta( $id, "オーブン対応", true ) . " / 食器洗浄機" . get_post_meta( $id, "食洗機対応", true ) . 
+                                                        (
+                                                            ( get_post_meta( $id, "対応機器備考", true ) != "" ) 
+                                                                ? "\n※" . get_post_meta( $id, "対応機器備考", true ) 
+                                                                : ""
+                                                        ) 
+                                                    : ""
+                                                ) 
+                                            : ""
+                                    ) . 
+                                    (
+                                        ( preg_match( '/hasami|toki|tajimi/', esc_url( $_SERVER[ 'REQUEST_URI' ] ) ) && get_post_meta( $id, "やきもの", true ) !== "やきものではない" ) 
+                                            ? "\n\n【お申し込み前に必ずお読みください】\nお届けする返礼品には個体差が生じる場合があります。\n下部記載の「個体差について」、「破損・欠陥について」を必ずご一読のうえ、お申し込みください。" . 
+                                                (
+                                                    preg_match( '/toki|tajimi/', esc_url($_SERVER[ 'REQUEST_URI' ] ) ) 
+                                                        ? "\n\n【色について】\n商品写真はできる限り実物の色に近づけるよう徹底しておりますが、 お使いのモニター設定、お部屋の照明等により実際の商品と色味が異なる場合がございます。" 
+                                                        : ""
+                                                ) 
+                                            : ""
+                                    )
+                                ) . 
+                                (
+                                    get_post_meta($id, "原料原産地", true) 
+                                        ? "\n\n【原料原産地】\n" . N2_Functions::special_str_convert(get_post_meta($id, "原料原産地", true)) 
+                                        : ""
+                                ) . 
+                                (
+                                    get_post_meta($id, "加工地", true) 
+                                        ? "\n\n【加工地】\n" . N2_Functions::special_str_convert(get_post_meta($id, "加工地", true)) 
+                                        : ""
+                                ),
                 '申込期日'      => get_post_meta($id, "申込期間", true),
                 '発送期日'        => get_post_meta($id, "配送期間", true),
                 '（必須）アレルギー表示'        => '',
-                '地場産品類型番号'        => get_post_meta($id, "地場産品類型", true) ? get_post_meta($id, "地場産品類型", true) . "|" . get_post_meta($id, "類型該当理由", true) : "",
-                '消費期限'        => (get_post_meta($id, "賞味期限", true) != "") ? "【賞味期限】\n" . N2_Functions::special_str_convert(get_post_meta($id, "賞味期限", true)) . ((get_post_meta($id, "消費期限", true) != "") ? "\n\n【消費期限】\n" . N2_Functions::special_str_convert(get_post_meta($id, "消費期限", true)) : "") : N2_Functions::special_str_convert(get_post_meta($id, "消費期限", true)),
+                '地場産品類型番号'        => get_post_meta($id, "地場産品類型", true) 
+                                                ? get_post_meta($id, "地場産品類型", true) . "|" . get_post_meta($id, "類型該当理由", true) 
+                                                : "",
+                '消費期限'        => (get_post_meta($id, "賞味期限", true) != "") 
+                                        ? "【賞味期限】\n" . N2_Functions::special_str_convert(get_post_meta($id, "賞味期限", true)) . 
+                                            (
+                                                (get_post_meta($id, "消費期限", true) != "") 
+                                                    ? "\n\n【消費期限】\n" . N2_Functions::special_str_convert(get_post_meta($id, "消費期限", true)) 
+                                                    : ""
+                                            ) 
+                                        : N2_Functions::special_str_convert(get_post_meta($id, "消費期限", true)),
                 '（必須）常温配送'        => (get_post_meta($id, "発送方法", true) == "常温") ? 1 : 0,
                 '（必須）冷蔵配送'        => (get_post_meta($id, "発送方法", true) == "冷蔵") ? 1 : 0,
                 '（必須）冷凍配送'        => (get_post_meta($id, "発送方法", true) == "冷凍") ? 1 : 0,
@@ -84,10 +150,9 @@ class N2_Choice {
                 '（必須）定期配送対応'        => (get_post_meta($id, "定期便", true) == 1 || get_post_meta($id, "定期便", true) == "") ? 0 : 1,
                 '（必須）クレジット決済限定'        => (get_post_meta($id, "クレジット決済限定", true) == "クレジット決済限定") ? 1 : 0,
                 'カテゴリー'        => "",
-                'お礼の品画像'        => mb_strtolower($arr[$id]['管理コード']) . ".jpg",
+                'お礼の品画像'        => mb_strtolower(strtoupper( get_post_meta( $id, "返礼品コード", true ) )) . ".jpg",
                 '受付開始日時'        => "2025/04/01 00:00",
                 '（条件付き必須）還元率（%）'        => 30,
-                '説明'        => "\n\n" . $opt['add_text'][get_bloginfo('name')],
             );
 
             // 内容を追加、または上書きするためのフック
