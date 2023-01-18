1676e641a4f2ae580bbc7754eac17b26e776c967
ryota-nakano <rnakanor@gmail.com>
2023-01-18 06:49:56 +0000

    :zap: 商品画像完成（仮）

--- template/forms/item-image.php
+++ template/forms/item-image.php
@@ -14,16 +14,26 @@ $style   = $args['style'];
 <div class="wrap">
 	<input type="hidden" name="<?php echo $name; ?>" :value="JSON.stringify(商品画像)">
 	<draggable v-model="商品画像" class="">
-		<div v-for="(attr, index) in 商品画像" class="d-inline-block m-1">
-			<img :src="attr.sizes.thumbnail.url || attr.sizes.thumbnail">
-			<div>
-				<span class="badge rounded-pill bg-dark">{{index+1}}</span>
-				<span role="button" @click="商品画像.splice(index, 1)" class="badge rounded-pill bg-dark">削除</span>
-				<span role="button" @click="商品画像.splice(index, 1)" class="badge rounded-pill bg-dark">拡大</span>
+		<div v-for="(attr, index) in 商品画像" class="d-inline-block position-relative details attachment">
+			<a :href="attr.url" class="d-block" target="_blank">
+				<img :src="attr.sizes.thumbnail.url || attr.sizes.thumbnail" style="cursor: move;">
+			</a>
+			<!-- 削除 -->
+			<div role="button" @click="商品画像.splice(index, 1)" class="check rounded-circle d-flex justify-content-center align-items-center">
+				<span class="dashicons dashicons-no-alt text-white"></span>
 			</div>
+			<a :href="attr.url" target="_blank"　download class="position-absolute bottom-0 end-0 badge rounded-pill bg-dark text-white">
+				▼ Download
+			</a>
 		</div>
 	</draggable>
-	<div class="button" @click="add_media">画像を追加</div>
+	<div @click="add_media" class="d-inline-block position-relative selected attachment position-relative">
+		<div style="padding-top: 100%;background: #c3c4c7;"></div>
+		<div class="position-absolute top-0 start-0 end-0 bottom-0 d-flex justify-content-center align-items-center">
+			<span class="dashicons dashicons-plus-alt2 text-white"></span>
+		</div>
+		<!-- /.position-absolute -->
+	</div>
 </div>
 <!-- /.button -->
 <!-- <div class="%1$s-image-block">
