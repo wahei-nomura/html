1d23af45522a9974830a50e35de9b1bbd6ffa87a
ryota-nakano <rnakanor@gmail.com>
2023-01-31 00:46:17 +0000

    :wrench: 一番下でスピナー復活してたので削除

--- inc/class-n2-setpost.php
+++ inc/class-n2-setpost.php
@@ -65,7 +65,7 @@ class N2_Setpost {
 					$('body').append('<div id="n2-loading" class="d-flex justify-content-center align-items-center vh-100 bg-white"><div class="spinner-border text-primary"></div></div>');
 					$(".edit-post-layout__metaboxes").ready(() => {
 						$("#wpwrap").show(1000);
-						$("#n2-loading").hide(1000);
+						$("#n2-loading").remove();
 						window.n2.field_value = <?php echo wp_json_encode( (array) N2_Functions::get_all_meta( $post ) ); ?>;
 						window.n2.field_list = <?php echo wp_json_encode( (array) array_keys( N2_Functions::get_all_meta( $post ) ) ); ?>;
 						
