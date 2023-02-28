4e05e887caaec0d7b143b815ae96f2208bbd5a4e
ryota-nakano <rnakanor@gmail.com>
2023-02-28 10:26:28 +0900

    :wrench: スチームシップへ送信ボタンの調整

--- src/ts/modules/admin-post-editor-save-as-pending-post.ts
+++ src/ts/modules/admin-post-editor-save-as-pending-post.ts
@@ -6,9 +6,11 @@
  */
 export default ($: any, target: string) => {
 	// ターゲットDOMが生成されてから
+	const n2 = window['n2'];
+	const wp = window['wp'];
 	$(target).ready(() => {
-		const status = window['wp'].data.select("core/editor").getEditedPostAttribute("status");
-		if ( ! window['n2'].current_user.roles.includes('jigyousya') || ! status.match(/draft/)) return
+		const status = wp.data.select("core/editor").getEditedPostAttribute("status");
+		if ( ! n2.current_user.roles.includes('jigyousya') || ! status.match(/draft/)) return
 
 		// 保存ボタン配置
 		$(target).prepend('<div id="n2-save-as-pending" class="btn btn-sm btn-primary d-flex align-items-center" title="スチームシップへ送信"><span></span>スチームシップへ送信</div>');
@@ -20,11 +22,18 @@ export default ($: any, target: string) => {
 			}
 			if ( ! confirm('スチームシップへ送信後の編集はできません。本当に送信しますか？') ) return;
 			$('#n2-save-as-pending span').attr('class', 'spinner-border spinner-border-sm me-2');
-			window['wp'].data.dispatch('core/editor').editPost({ status: 'pending' });
-			window['wp'].data.dispatch('core/editor').savePost().then(()=>{
+			wp.data.dispatch('core/editor').editPost({ status: 'pending' });
+			wp.data.dispatch('core/editor').savePost().then(()=>{
 				$('#n2-save-as-pending span').attr('class', 'dashicons dashicons-saved me-2');
 				location.reload();
 			})
+		});
+		wp.data.subscribe(()=>{
+			if ( $('.edit-post-layout__metaboxes [required]').serializeArray().find(v=>!v.value) ) {
+				$('#n2-save-as-pending').addClass('opacity-25');
+			} else {
+				$('#n2-save-as-pending').removeClass('opacity-25');
+			}
 		});
 	})
 }
