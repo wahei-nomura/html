6677fe148de6f0b41661c256828b1247263f672b
ryota-nakano <rnakanor@gmail.com>
2023-03-29 13:50:52 +0900

    :wrench: 全商品ディレクトリIDとタグIDあたりのUIが怪しいので調整

--- src/ts/modules/admin-post-editor-vue.ts
+++ src/ts/modules/admin-post-editor-vue.ts
@@ -26,13 +26,14 @@ export default $ => {
 			group: '',
 			list: [],
 		};
-
+		
 		this.寄附金額 = await this.calc_donation(this.価格,this.送料,this.定期便);
 		this.show_submit();
 		// 発送サイズ・発送方法をダブル監視
 		this.$watch(
 			() => {
 				return {
+					全商品ディレクトリID: this.$data.全商品ディレクトリID.text,
 					価格: this.$data.価格,
 					発送方法: this.$data.発送方法,
 					発送サイズ: this.$data.発送サイズ,
@@ -41,6 +42,14 @@ export default $ => {
 				}
 			},
 			async function(newVal, oldVal) {
+				// タグIDのリセット
+				if ( newVal.全商品ディレクトリID != oldVal.全商品ディレクトリID && this.タグID.text.length ) {
+					if ( confirm('全商品ディレクトリIDが変更されます。\nそれに伴い入力済みのタグIDをリセットしなければ楽天で地味にエラーがでます。\n\nタグIDをリセットしてよろしいでしょうか？') ) {
+						this.タグID.list = [];
+						this.タグID.text = '';
+					}
+				}
+				// 寄附金額の算出
 				const size = [
 					newVal.発送サイズ,
 					newVal.発送方法 != '常温' ? 'cool' : ''
@@ -50,6 +59,7 @@ export default $ => {
 				}
 				this.送料 = newVal.送料;
 				this.寄附金額 = await this.calc_donation(newVal.価格,this.送料,newVal.定期便);
+				// 保存ボタン
 				this.show_submit();
 			},
 		);
@@ -124,7 +134,7 @@ export default $ => {
 			images.open();
 		},
 		// 楽天の全商品ディレクトリID取得（タグIDでも利用）
-		async get_genreid( tagid_reset = false ){
+		async get_genreid(){
 			const settings = {
 				url: '//app.rakuten.co.jp/services/api/IchibaGenre/Search/20140222',
 				data: {
@@ -133,14 +143,6 @@ export default $ => {
 				},
 			};
 			this.全商品ディレクトリID.list = await $.ajax(settings);
-			if ( tagid_reset && this.タグID.text ) {
-				if ( confirm('全商品ディレクトリIDが変更されます。\nそれに伴い入力済みのタグIDをリセットしなければ楽天で地味にエラーがでます。\n\nタグIDをリセットしてよろしいでしょうか？') ) {
-					this.タグID.list = [];
-					this.タグID.text = '';
-				} else {
-					this.タグID.text = this.タグID.text;
-				}
-			}
 		},
 		// タグIDと楽天SPAカテゴリーで利用
 		update_textarea(id, target = 'タグID', delimiter = '/'){
