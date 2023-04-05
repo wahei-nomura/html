9fabc19464927c1a159569a9b585f64c440d13ac
HiroakiKatada <hiroaki.katada@steamship.co.jp>
2023-04-05 17:40:44 +0900

    脳死pushでインデント爆死

--- inc/api/class-n2-output-gift-api.php
+++ inc/api/class-n2-output-gift-api.php
@@ -1,27 +1,27 @@
 <?php
+
 /**
  * N2返礼品出力API
  *
  * @package neoneng
  */
 
- if ( class_exists( 'N2_Output_Gift_API' ) ) {
+if (class_exists('N2_Output_Gift_API')) {
 	new N2_Output_Gift_API();
 	return;
 }
 
 class N2_Output_Gift_API {
 
-public function __construct() {
-	add_action( 'wp_ajax_n2_output_gift_api', array( $this, 'get' ) );
-}
+	public function __construct() {
+		add_action('wp_ajax_n2_output_gift_api', array($this, 'get'));
+	}
 
-/**
- * 外部用API
- */
-public function get() {
-	echo 'hello, world';
-	exit;
-}
-
+	/**
+	 * 外部用API
+	 */
+	public function get() {
+		echo 'hello, world';
+		exit;
+	}
 }
