67671fe76839782d8036ed304a7d8323a97b3a99
HiroakiKatada <hiroaki.katada@steamship.co.jp>
2023-04-14 18:09:49 +0900

    クエリ・可読性改善＆出しわけ処理記述の途中、週末なので一旦push

--- inc/api/class-n2-output-gift-api.php
+++ inc/api/class-n2-output-gift-api.php
@@ -45,47 +45,23 @@ class N2_Output_Gift_API {
 		$query = <<<SELECT_SQL
 		SELECT
 			posts.post_title as title,
-			postmeta.寄附金額,
-			postmeta.返礼品コード,
-			postmeta.消費期限,
-			postmeta.賞味期限,
-			postmeta.説明文,
-			postmeta.電子レンジ対応,
-			postmeta.オーブン対応,
-			postmeta.食洗機対応,
-			postmeta.内容量・規格等,
-			postmeta.発送方法,
-			postmeta.定期便,
-			postmeta.包装対応,
-			postmeta.のし対応,
-			postmeta.配送期間
+			postmeta.meta_key,
+			postmeta.meta_value
 		FROM
 			wp_{$site_id}_posts as posts
-		INNER JOIN (
-			SELECT 
-				post_id,
-				max(CASE WHEN meta_key = '寄附金額' THEN meta_value END) AS 寄附金額,
-				max(CASE WHEN meta_key = '返礼品コード' THEN meta_value END) AS 返礼品コード,
-				max(CASE WHEN meta_key = '消費期限' THEN meta_value END) AS 消費期限,
-				max(CASE WHEN meta_key = '賞味期限' THEN meta_value END) AS 賞味期限,
-				max(CASE WHEN meta_key = '説明文' THEN meta_value END) AS 説明文,
-				max(CASE WHEN meta_key = '電子レンジ対応' THEN meta_value END) AS 電子レンジ対応,
-				max(CASE WHEN meta_key = 'オーブン対応' THEN meta_value END) AS オーブン対応,
-				max(CASE WHEN meta_key = '食洗機対応' THEN meta_value END) AS 食洗機対応,
-				max(CASE WHEN meta_key = '内容量・規格等' THEN meta_value END) AS 内容量・規格等,
-				max(CASE WHEN meta_key = '発送方法' THEN meta_value END) AS 発送方法,
-				max(CASE WHEN meta_key = '定期便' THEN meta_value END) AS 定期便,
-				max(CASE WHEN meta_key = '包装対応' THEN meta_value END) AS 包装対応,
-				max(CASE WHEN meta_key = 'のし対応' THEN meta_value END) AS のし対応,
-				max(CASE WHEN meta_key = '配送期間' THEN meta_value END) AS 配送期間
-			FROM
-				wp_{$site_id}_postmeta
-			GROUP BY
-				post_id ) as postmeta
+		INNER JOIN
+			wp_{$site_id}_postmeta as postmeta
 		ON
-			postmeta.post_id = posts.id
+			posts.id = postmeta.post_id
 		WHERE
-		    postmeta.返礼品コード = %s;
+			posts.id in (
+				SELECT
+					post_id
+				FROM
+					wp_{$site_id}_postmeta
+				WHERE
+					meta_value = %s
+			);
 		SELECT_SQL;
 
 		// サニタイズした返礼品コードをクエリの「%s」の部分に入れてクエリを完成させる
@@ -93,6 +69,20 @@ class N2_Output_Gift_API {
 
 		// SQLクエリを実行し、結果を連想配列で取得
 		$results = $wpdb->get_results( $query, ARRAY_A );
+
+		$gift = [];
+		foreach ( $results as $result ) {
+			$title = $result['title'];
+			$meta_key = $result['meta_key'];
+			$meta_value = $result['meta_value'];
+
+			if ( ! isset( $gift[ $title ] ) ) {
+				$gift[ $title ] = [ 'title' => $title ];
+			}
+			$gift[ $title ][ $meta_key ] = $meta_value;
+		}
+
+		$results = array_values( $gift );
 
 		// 結果をJSON形式に変換して出力
 		header( 'Content-Type: application/json' );
@@ -103,6 +93,83 @@ class N2_Output_Gift_API {
 	 * 外部用API
 	 */
 	public function get_by_nopriv_user() {
-		echo 'これはno_priv用のやつ';
+		global $wpdb;
+
+		// URLの末尾からskuを取得、サニタイズ
+		$sku = isset( $_GET['sku'] ) ? sanitize_text_field( $_GET['sku'] ) : '';
+
+		// skuが空の場合、処理を終了する
+		if ( '' === $sku ) :
+			exit;
+		endif;
+
+		// 自治体コードを取得
+		global $n2;
+		$site_id = $n2->site_id;
+
+		// テーブルから情報を取得するSQLクエリを準備
+		$query = <<<SELECT_SQL
+		SELECT
+			posts.post_title as title,
+			postmeta.meta_key,
+			postmeta.meta_value
+		FROM
+			wp_{$site_id}_posts as posts
+		INNER JOIN
+			wp_{$site_id}_postmeta as postmeta
+		ON
+			posts.id = postmeta.post_id
+		WHERE
+			postmeta.meta_key in (
+				'寄附金額',
+				'返礼品コード',
+				'消費期限',
+				'賞味期限',
+				'説明文',
+				'電子レンジ対応',
+				'オーブン対応',
+				'食洗機対応',
+				'内容量・規格等',
+				'発送方法',
+				'定期便',
+				'包装対応',
+				'のし対応',
+				'配送期間'
+				)
+		AND
+			posts.id in (
+				SELECT
+					post_id
+				FROM
+					wp_{$site_id}_postmeta
+				WHERE
+					meta_value = %s
+			);
+		SELECT_SQL;
+
+		// サニタイズした返礼品コードをクエリの「%s」の部分に入れてクエリを完成させる
+		$query = $wpdb->prepare( $query, $sku );
+
+		// SQLクエリを実行し、結果を連想配列で取得
+		$results = $wpdb->get_results( $query, ARRAY_A );
+
+		$gift = [];
+		foreach ( $results as $result ) {
+			$title = $result['title'];
+			$meta_key = $result['meta_key'];
+			$meta_value = $result['meta_value'];
+
+			if ( ! isset( $gift[ $title ] ) ) {
+				$gift[ $title ] = [ 'title' => $title ];
+			}
+			$gift[ $title ][ $meta_key ] = $meta_value;
+		}
+
+		$results = array_values( $gift );
+
+		// 結果をJSON形式に変換して出力
+		header( 'Content-Type: application/json' );
+		echo wp_json_encode( $results );
+		exit;
 	}
 }
