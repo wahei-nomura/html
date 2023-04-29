efa343f43242fefc5ec608984233e84d4fd1ba64
ryota-nakano <rnakanor@gmail.com>
2023-04-29 10:47:19 +0900

    :wrench: エクスポートベースのクラスほぼ完成。あと、フックをつける。

--- inc/item-export/class-n2-item-export-base.php
+++ inc/item-export/class-n2-item-export-base.php
@@ -22,9 +22,9 @@ class N2_Item_Export_Base {
 	 *
 	 * @var array
 	 */
-	public $settings = array(
-		'name'          => 'n2_export_base.tsv',
-		'delimiter'     => "\t",
+	protected $settings = array(
+		'name'          => 'n2_export_base.csv',
+		'delimiter'     => ',',
 		'charset'       => 'utf-8',
 		'header_string' => '', // 基本は自動設定、falseでヘッダー文字列無し
 	);
@@ -34,7 +34,7 @@ class N2_Item_Export_Base {
 	 *
 	 * @var array
 	 */
-	public $data = array(
+	protected $data = array(
 		'header'  => array(),
 		'n2field' => array(),
 		'n2data'  => array(),
@@ -54,13 +54,13 @@ class N2_Item_Export_Base {
 	 */
 	public function export() {
 		$defaults = array(
-			'type' => 'download',
+			'mode' => 'download',
 		);
 		// デフォルト値を$_GETで上書き
 		$get = wp_parse_args( $_GET, $defaults );
 
-		if ( ! method_exists( 'N2_Item_Export_Base', $get['type'] ) ) {
-			echo "「{$get['type']}」メソッドは存在しません。";
+		if ( ! method_exists( 'N2_Item_Export_Base', $get['mode'] ) ) {
+			echo "「{$get['mode']}」メソッドは存在しません。";
 			exit;
 		}
 
@@ -71,21 +71,25 @@ class N2_Item_Export_Base {
 		// 独自のデータをセット
 		$this->set_header();
 		$this->set_data();
-		$this->set_header_string();
-		$this->set_string();
 
-		$this->{$get['type']}();
+		// データを文字列に
+		$this->set_header_string();
+		$this->set_data_string();
+
+		$this->{$get['mode']}();
 	}
 
 	/**
 	 * N2カスタムフィールド全設定項目をセット
 	 */
-	protected function set_n2field() {
+	private function set_n2field() {
 		global $n2;
 		$n2field = array(
 			...array_keys( $n2->custom_field['スチームシップ用'] ),
 			...array_keys( $n2->custom_field['事業者用'] ),
 		);
+		// 重複削除
+		$n2field = array_unique( $n2field );
 		// 商品画像とN1zipは使いようがないので対象から外す
 		$exclusion = array(
 			'商品画像',
@@ -100,13 +104,21 @@ class N2_Item_Export_Base {
 	/**
 	 * N2データ取得してセット
 	 */
-	protected function set_n2data() {
+	private function set_n2data() {
 		$n2data = array();
-		// $ids  = filter_input( INPUT_POST, 'ids' );
-		// $ids  = explode( ',', $ids );
-		$ids = get_posts( 'fields=ids&post_status=any&numberposts=10' );
+		// POSTされた投稿ID
+		$ids = filter_input( INPUT_POST, 'ids' );
+		$ids = $ids ? explode( ',', $ids ) : get_posts( 'post_status=any&fields=ids&numberposts=-1' );
 		foreach ( $ids as $id ) {
-			$n2data[ $id ]['タイトル'] = get_the_title( $id );
+			$post = get_post( $id );
+			// タイトル追加
+			$n2data[ $id ]['タイトル'] = $post->post_title;
+			// 事業者コード追加
+			$n2data[ $id ]['事業者コード'] = get_user_meta( $post->post_author, 'last_name', true );
+			// 事業者名追加
+			$n2data[ $id ]['事業者名'] = get_user_meta( $post->post_author, 'first_name', true );
+			// 投稿ステータス追加
+			$n2data[ $id ]['ステータス'] = get_post_status( $id );
 			// n2fieldのカスタムフィールド全取得
 			foreach ( $this->data['n2field'] as $key ) {
 				$meta = get_post_meta( $id, $key, true );
@@ -121,14 +133,20 @@ class N2_Item_Export_Base {
 	}
 
 	/**
-	 * ヘッダーの作成
+	 * ヘッダー配列の作成（基本的に拡張で上書きする）
 	 */
 	protected function set_header() {
-		$this->data['header'] = $this->data['n2field'];
+		$this->data['header'] = array(
+			'タイトル',
+			'事業者コード',
+			'事業者名',
+			'ステータス',
+			...$this->data['n2field'],
+		);
 	}
 
 	/**
-	 * 内容を配列で作成
+	 * 内容を配列で作成（基本的に拡張で上書きする）
 	 */
 	protected function set_data() {
 		$data = array();
@@ -150,7 +168,7 @@ class N2_Item_Export_Base {
 	/**
 	 * ヘッダー文字列生成
 	 */
-	protected function set_header_string() {
+	private function set_header_string() {
 		if ( false === $this->settings['header_string'] ) {
 			$this->settings['header_string'] = '';
 		} else {
@@ -161,18 +179,36 @@ class N2_Item_Export_Base {
 	/**
 	 * 配列をCSV・TSVに変換
 	 */
-	protected function set_string() {
+	private function set_data_string() {
 		$str = $this->settings['header_string'];
 		foreach ( $this->data['data'] as $key => $value ) {
-			$str .= '"' . implode( "\"{$this->settings['delimiter']}\"", $value ) . '"' . PHP_EOL;
+			// ダブルクオーテーションのエスケープ処理
+			$value = array_map( fn( $v ) => str_replace( '"', '""', $v ), $value );
+			// その他文字列の痴漢
+			$value = array_map( array( $this, 'special_str_convert' ), $value );
+			$str  .= '"' . implode( "\"{$this->settings['delimiter']}\"", $value ) . '"' . PHP_EOL;
 		}
 		$this->data['string'] = $str;
 	}
 
 	/**
+	 * 文字列の置換（拡張で上書き可能）
+	 *
+	 * @param string $str 文字列
+	 * @return string $str 置換後の文字列
+	 */
+	protected function special_str_convert( $str ) {
+		/**
+		 * hook n2_item_export_base_special_str_convert
+		 */
+		$str = apply_filters( mb_strtolower( get_class( $this ) ) . '_special_str_convert', $str );
+		return $str;
+	}
+
+	/**
 	 * ダウンロード
 	 */
-	protected function download() {
+	private function download() {
 		$this->data['string'] = mb_convert_encoding( $this->data['string'], $this->settings['charset'], 'utf-8' );
 		header( 'Content-Type: application/octet-stream' );
 		header( "Content-Disposition: attachment; filename={$this->settings['name']}" );
@@ -183,13 +219,13 @@ class N2_Item_Export_Base {
 	/**
 	 * スプレットシートに値貼り付け用
 	 */
-	protected function spreadsheet() {
+	private function spreadsheet() {
 		// ヘッダーを強制的に付与
 		if ( '' === $this->settings['header_string'] ) {
 			$this->set_header_string();
 		}
-		$this->settings['delimiter']     = "\t"; // デリミタをタブに変更
-		$this->set_string();
+		$this->settings['delimiter'] = "\t"; // デリミタをタブに変更
+		$this->set_data_string();
 		?>
 		<title>スプレットシートに値貼り付け</title>
 		<pre><?php echo esc_html( $this->data['string'] ); ?></pre>
@@ -200,7 +236,7 @@ class N2_Item_Export_Base {
 	/**
 	 * デバッグ用
 	 */
-	protected function debug() {
+	private function debug() {
 		echo '<pre>';
 		print_r( $this->settings );
 		print_r( $this->data );
