bd51db9d600cbde03578f26d3930342c3ae80d3e
Taiki-ss <taiki.ishida.ss@gmail.com>
2023-05-10 10:22:44 +0900

    :+1:[fix] 不具合フォームリンクをもうちょい邪魔なならない感じに

--- src/ts/admin-qaform.ts
+++ src/ts/admin-qaform.ts
@@ -4,7 +4,7 @@ jQuery(function($){
 	 */
 
 	const n2 = window['n2'];
-	const formLink = $('<a href="https://docs.google.com/forms/d/e/1FAIpQLScbze4H3puDboZ0zEZ_vfx7EzpiV0KJFeKFjFnGjymxqekw5Q/viewform" target="_blank"><span class="dashicons dashicons-warning"></span>システムの不具合は<br>こちら</a>');
+	const formLink = $('<a href="https://docs.google.com/forms/d/e/1FAIpQLScbze4H3puDboZ0zEZ_vfx7EzpiV0KJFeKFjFnGjymxqekw5Q/viewform" target="_blank"><span class="dashicons dashicons-warning"></span>システムの不具合はこちら</a>');
 
 	formLink.css({
 		'position': 'fixed ',
@@ -14,16 +14,13 @@ jQuery(function($){
 		'display':'flex ',
 		'justify-content':'center ',
 		'align-items':'center ',
-		'flex-direction': 'column',
 		'text-align':'center ',
 		'color': '#fff ',
 		'font-size': '13px',
 		'background-color': '#b2292c ',
-		'border-radius': '50% ',
+		'border-radius': '4px',
 		'box-shadow': '0 3px 5px rgba(0, 0, 0, 0.3)',
-		'width': '130px ',
-		'height': '90px ',
-		'padding':  '2px ',
+		'padding':  '4px ',
 		'text-decoration':'none',
 	});
 
