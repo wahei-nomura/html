dace37a9c85746f1485a557e457e40746bd0aa1f
ryota-nakano <rnakanor@gmail.com>
2023-05-18 18:19:41 +0900

    :wrench: LHのエクスポート、あと送料反映の意味不明な部分とエラー処理

--- inc/item-export/class-n2-item-export-ledghome.php
+++ inc/item-export/class-n2-item-export-ledghome.php
@@ -53,34 +53,41 @@ class N2_Item_Export_Ledghome extends N2_Item_Export_Base {
 	 */
 	protected function walk_values( &$val, $index, $n2values ) {
 		global $n2;
+		$data = array();
+		for ( $i = 1; $i <= $n2values['定期便']; $i++ ) {
+			// 返礼品コード
+			$item_code = $n2values['返礼品コード'] . ( $n2values['定期便'] > 1 ? "_{$i}/{$n2values['定期便']}" : '' );
+			// データ配列
+			$data[ $i ] = match ( $val ) {
+				'謝礼品番号' => $item_code,// 定期便の場合は「_1/12」をつける
+				'謝礼品名' => "{$item_code} " . ( $n2values['LH表示名'] ?: $n2values['タイトル'] ),// LH表示名 > タイトル
+				'配送名称' => "{$item_code} " . ( $n2values['配送伝票表示名'] ?: $n2values['タイトル'] ),// 配送伝票表示名 > タイトル
+				'ふるさとチョイス名称' => "{$n2values['タイトル']} [{$n2values['返礼品コード']}]",
+				'楽天名称' => "【ふるさと納税】{$n2values['タイトル']} [{$n2values['返礼品コード']}]",
+				'事業者' => $n2values['事業者名'],
+				'謝礼品カテゴリー' => $n2values['LHカテゴリー'],
+				'セット内容' => $n2values['内容量・規格等'],
+				'謝礼品紹介文' => $n2values['説明文'],
+				'ステータス' => '受付中',
+				'状態' => '表示',
+				'寄附設定金額' => $i > 1 ? 0 : $n2values['寄附金額'],// 定期便の場合は１回目のみ
+				'価格（税込み）' => $n2values['価格'],// 1回目に全部含めるかどうかの設定値を使う
+				'送料' => $n2values['送料'],
 
-		// preg_matchで判定
-		$data = match ( $val ) {
-			'謝礼品番号' => $n2values['返礼品コード'],// 定期便の処理まだ
-			'謝礼品名' => '',// ムズっ！
-			'配送名称' => '',// ムズっ！
-			'ふるさとチョイス名称' => '',
-			'楽天名称' => '',
-			'事業者' => $n2values['事業者名'],
-			'謝礼品カテゴリー' => $n2values['LHカテゴリー'],
-			'セット内容' => $n2values['内容量・規格等'],
-			'謝礼品紹介文' => $n2values['説明文'],
-			'ステータス' => '受付中',
-			'状態' => '表示',
-			'寄附設定金額' => '',// 定期便がムズい
-			'価格（税込み）' => '',// 定期便がムズい
-			'送料' => $n2values['送料'],
-			'送料反映' => '',// ムズっ！
-			'発送方法' => $n2values['発送方法'],
-			'取り扱い方法' => implode( $n2values['取り扱い方法'] ),
-			'申込可能期間' => '通年',
-			'自由入力欄1' => wp_date( 'Y/m/d' ) . "：{$n2->current_user->data->display_name}",
-			'自由入力欄2' => $n2values['送料'],
-			'配送サイズコード' => is_numeric( $n2values['発送サイズ'] ) ? $n2values['発送サイズ'] : '',
-			'地場産品類型' => '',// ムズぅ！
-			'類型該当理由' => $n2values['類型該当理由'],
-			default => '',
-		};
+				// マジ卍
+				'送料反映' => '',// ムズっ！
+	
+				'発送方法' => $n2values['発送方法'],
+				'取り扱い方法' => implode( $n2values['取り扱い方法'] ),
+				'申込可能期間' => '通年',
+				'自由入力欄1' => wp_date( 'Y/m/d' ) . "：{$n2->current_user->data->display_name}",
+				'自由入力欄2' => $n2values['送料'],
+				'配送サイズコード' => is_numeric( $n2values['発送サイズ'] ) ? $n2values['発送サイズ'] : '',
+				'地場産品類型' => implode( 'ー', mb_str_split( mb_convert_kana( $n2values['地場産品類型'], 'KA' ) ) ),// 全角（「８ーイ」形式）
+				'類型該当理由' => $n2values['類型該当理由'],
+				default => '',
+			};
+		}
 		/**
 		 * [hook] n2_item_export_ledghome_walk_values
 		 */
@@ -102,16 +109,16 @@ class N2_Item_Export_Ledghome extends N2_Item_Export_Base {
 			$this->add_error( $n2values['id'], "「{$name}」がありません。" );
 		}
 		// 文字数制限エラー
-		$len       = mb_strlen( $value );// $valueの文字数
-		$maxlength = array(
-			// 40 => '謝礼品紹介文',
-		);
-		foreach ( $maxlength as $max => $pattern ) {
-			if ( preg_match( "/{$pattern}/", $name ) && $len > $max ) {
-				$over = $len - $max;
-				$this->add_error( $n2values['id'], "<div title='{$value}'>「{$name}」の文字数が{$over}文字多いです。</div>" );
-			}
-		}
+		// $len       = mb_strlen( $value );// $valueの文字数
+		// $maxlength = array(
+		// 	// 40 => '謝礼品紹介文',
+		// );
+		// foreach ( $maxlength as $max => $pattern ) {
+		// 	if ( preg_match( "/{$pattern}/", $name ) && $len > $max ) {
+		// 		$over = $len - $max;
+		// 		$this->add_error( $n2values['id'], "<div title='{$value}'>「{$name}」の文字数が{$over}文字多いです。</div>" );
+		// 	}
+		// }
 		return $value;
 	}
 	/**
