764b4cf36f7b285f5bdb4bc5925207d56002056a
ryota-nakano <rnakanor@gmail.com>
2023-05-22 11:41:40 +0900

    :wrench: やっぱボタン分けたが良さげ

--- template/export-btns.php
+++ template/export-btns.php
@@ -25,7 +25,8 @@
 		<a href="#" class="button button-primary sisbtn" id="n2_item_export_base">エクスポート</a>
 		<a href="#" class="button button-primary sisbtn" id="n2_item_export_ledghome">Ledg HOME</a>
 		<a href="#" class="button button-primary sisbtn" id="n2_item_export_furusato_choice">チョイス</a>
-		<a href="#" class="button button-primary sisbtn" id="n2_item_export_rakuten">楽天</a>
+		<a href="#" class="button button-primary sisbtn" id="n2_item_export_rakuten">楽天 item</a>
+		<a href="#" class="button button-primary sisbtn" id="n2_item_export_rakuten_select">楽天 select</a>
 		<a href="#" class="button button-primary siserror" id="error_log">楽天エラーログ</a>
 		<a href="#" class="button button-primary dlbtn" id="download_by_url">画像をダウンロード</a>
 		<button type="button" class="button button-primary" id="bulk_update_status">ステータス一括変更</button>
