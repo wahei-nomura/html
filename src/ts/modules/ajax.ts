2e7543f5dc538fa8aa0fd35cd46a359f1ef7ebf1
hirotoki3193 <1stlevi777@gmail.com>
2023-06-05 11:05:11 +0900

    :recycle:[refactor] ajaxの関数をリファクタ

--- src/ts/modules/ajax.ts
+++ src/ts/modules/ajax.ts
@@ -1,8 +1,42 @@
 import { prefix, neoNengPath, ajaxUrl } from "./functions";
+import $ from 'jquery'
 
 /**
  * 返礼品一覧ページで使用するAjax用のファイル
  */
+
+// チェックが入った返礼品のidを配列で返す
+export const getIds = (): string => {
+	const checkbox = $.makeArray($('input[name="post[]"]'));
+	const checked = checkbox.flatMap((v) =>
+		$(v).prop("checked") ? $(v).val() : []
+	);
+	return checked.length ? checked.join() : "";
+};
+// POST送信してURLを別タブで開く
+export const openByPostAnotherPage = (
+	url: string,
+	btnName: string,
+	ids: string
+): Window => {
+	if (!ids) return;
+	const target_tab = 'n2_another'
+	const win = window.open("about:blank", target_tab);
+	const form = document.createElement("form");
+	const body = document.getElementsByTagName("body")[0];
+	form.action = url + "?action=" + btnName;
+	form.method = "post";
+	form.target = target_tab
+	const input = document.createElement("input");
+	input.type = "hidden";
+	input.name = btnName;
+	input.value = ids;
+	form.appendChild(input);
+	body.appendChild(form);
+	form.submit();
+	body.removeChild(form);
+	return win;
+};
 
 jQuery(function ($) {
 	$(".sisbtn").on("click", (e) => {
@@ -17,38 +51,6 @@ jQuery(function ($) {
 		console.log(getIds());
 	});
 
-	// チェックが入った返礼品のidを配列で返す
-	const getIds = (): string => {
-		const checkbox = $.makeArray($('input[name="post[]"]'));
-		const checked = checkbox.flatMap((v) =>
-			$(v).prop("checked") ? $(v).val() : []
-		);
-		return checked.length ? checked.join() : "";
-	};
-
-	// POST送信してURLを別タブで開く
-	const openByPostAnotherPage = (
-		url: string,
-		btnName: string,
-		ids: string
-	): Window => {
-		if (!ids) return;
-		const win = window.open("about:blank", 'n2_another');
-		const form = document.createElement("form");
-		const body = document.getElementsByTagName("body")[0];
-		form.action = url + "?action=" + btnName;
-		form.method = "post";
-		form.target = "n2_another"
-		const input = document.createElement("input");
-		input.type = "hidden";
-		input.name = btnName;
-		input.value = ids;
-		form.appendChild(input);
-		body.appendChild(form);
-		form.submit();
-		body.removeChild(form);
-		return win;
-	};
 	// POST送信してURLを開く
 	const openByPost = (
 		url: string,
