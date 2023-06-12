464dacb92b6e1ce59831e32e03cb3cb18148097f
ryota-nakano <rnakanor@gmail.com>
2023-06-12 19:18:35 +0900

    :wrench: アレルゲンの更新とAPI更新を一撃で完了させる

--- inc/class-n2-change-allergen.php
+++ inc/class-n2-change-allergen.php
@@ -5,21 +5,21 @@
  * @package neoneng
  */
 
-if ( class_exists( 'N2_Changeallergen' ) ) {
-	new N2_Changeallergen();
+if ( class_exists( 'N2_Change_Allergen' ) ) {
+	new N2_Change_Allergen();
 	return;
 }
 
 /**
- * N2_Changeallergen
+ * N2_Change_Allergen
  */
-class N2_Changeallergen {
+class N2_Change_Allergen {
 
 	/**
 	 * コンストラクタ
 	 */
 	public function __construct() {
-		add_action( 'wp_ajax_changeallergen', array( $this, 'change_allergen' ) );
+		add_action( 'wp_ajax_n2_change_allergen', array( $this, 'change_allergen' ) );
 	}
 
 	/**
@@ -28,30 +28,17 @@ class N2_Changeallergen {
 	 * @return void
 	 */
 	public function change_allergen() {
-		global $n2;
-		$all_ids = get_posts(array(
-			'posts_per_page' => -1,
-			'post_status' => 'any',
-			'fields' => 'ids',
-		));
-		$new_allergen_array = [];
-		foreach($all_ids as $id_key => $all_id){
-			$original_allergen_array = get_post_meta( $all_id, 'アレルゲン', true );
-			foreach($original_allergen_array as $original_allergen){
-				if(isset($original_allergen['label'])){ // 「label」がある場合だけ処理
-					if( $original_allergen['label'] != '食品ではない' && $original_allergen['label'] != 'アレルゲンなし食品' ){
-						$is_near  = false;
-						preg_match( '/ピーナ/', $original_allergen['label'], $is_near ); // 「ピーナツ」や「ピーナッツ」を「落花生」にまるめ
-						if( $is_near ){
-							$original_allergen['label'] = '落花生';
-						}
-						$new_allergen_array[$all_id][] = $original_allergen['label'];
-					}
-				}
+		foreach ( get_posts( 'posts_per_page=-1&post_status=any' ) as $post ) {
+			$allergens = get_post_meta( $post->ID, 'アレルゲン', true );
+			if ( empty( $allergens ) ) {
+				continue;
 			}
-			if(is_array( $new_allergen_array[$all_id] )){
-				update_post_meta($all_id, 'アレルゲン', $new_allergen_array[$all_id]);
-			}
-		}		
+			$allergens = array_map( fn( $v ) => isset( $v['label'] ) ? $v['label'] : $v, $allergens );// labelありの場合はlabelのみ、それ以外はそのまま
+			$allergens = str_replace( '（ピーナッツ）', '', $allergens );// チョイスの新仕様の「落花生（ピーナッツ）」は「落花生」
+			update_post_meta( $post->ID, 'アレルゲン', $allergens );// アレルゲン更新
+			wp_insert_post( $post );// API更新フック発火
+		}
+		echo 'アレルゲン更新と、ついでにAPI更新完了！';
+		exit;
 	}
 }
