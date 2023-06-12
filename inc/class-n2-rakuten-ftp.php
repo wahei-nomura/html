a021b06d718ae046cca65f1d674e0503cde66fbf
ryota-nakano <rnakanor@gmail.com>
2023-06-12 13:44:47 +0900

    :poop: スピード重視の超突貫楽天FTPなので、後で絶対に修正必要

--- inc/class-n2-rakuten-ftp.php
+++ inc/class-n2-rakuten-ftp.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * class-n2-rakuten-ftp.php
+ * 全体的に超絶突貫なので、後で作り直す
  *
  * @package neoneng
  */
@@ -25,47 +26,247 @@ class N2_Rakuten_FTP {
 	public function __construct() {
 		add_action( 'wp_ajax_n2_rakuten_ftp', array( $this, 'ftp' ) );
 		add_action( 'admin_menu', array( $this, 'add_menu' ) );
+		add_action( 'wp_ajax_n2_upload_to_rakuten', array( $this, 'upload_to_rakuten' ) );
 	}
 
 	/**
 	 * N2 SYNC　メニューの追加
 	 */
 	public function add_menu() {
-		add_menu_page( '楽天FTP', '楽天FTP', 'ss_crew', 'n2-rakuten-ftp', array( $this, 'ftp_ui' ), 'dashicons-admin-site-alt3' );
+		global $n2;
+		if ( isset( $n2->portal_setting['楽天'] ) ) {
+			add_menu_page( '楽天FTP', '楽天FTP', 'ss_crew', 'n2_rakuten_ftp_upload', array( $this, 'ftp_ui' ), 'dashicons-admin-site-alt3' );
+			add_submenu_page( 'n2_rakuten_ftp_upload', '楽天エラーログ', '楽天エラーログ', 'ss_crew', 'n2_rakuten_error_log', array( $this, 'ftp_ui' ) );
+		}
 	}
 
 	/**
 	 * FTP UI
 	 */
 	public function ftp_ui() {
-		$template = isset( $_GET['tab'] ) ? "ftp_ui_{$_GET['tab']}" : 'update_server';
+		$template = str_replace( 'n2_rakuten_', '', $_GET['page'] );
 		?>
 		<div class="wrap">
-			<h1>商品CSV・画像UP用サーバー</h1>
+			<h1>楽天FTP</h1>
 			<?php echo $this->$template(); ?>
 		</div>
 		<?php
 	}
 
 	/**
-	 * アップデートサーバーFTP
+	 * アップロード（超突貫）
 	 */
-	public function update_server() {
+	public function ftp_upload() {
+		?>
+		<div style="clear:both;padding:10px 0;">
+			<form action="admin-ajax.php" target="_blank" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="n2_upload_to_rakuten">
+				<input type="hidden" name="judge" value="ftp_img">
+				<input name="ftp_img[]" type="file" multiple="multiple">
+				<input type="submit" class="button" value="楽天に商品画像転送">
+			</form>
+		</div>
+		<div style="clear:both;padding:10px 0;">
+			<form action="admin-ajax.php" target="_blank" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="n2_upload_to_rakuten">
+				<input type="hidden" name="judge" value="ftp_file">
+				<input name="ftp_file[]" type="file" multiple="multiple">
+				<input type="submit" class="button" value="楽天に商品CSV転送">
+			</form>
+		</div>
+		<?php
+	}
+	/**
+	 * 楽天への転送機能（超突貫）
+	 *
+	 * @return void
+	 */
+	public function upload_to_rakuten() {
+
+		// 各種設定読み込み
+		global $n2;
+		$rakuten = $n2->portal_setting['楽天'];
+		// print_r($rakuten);
+		// setlocale(LC_ALL, 'ja_JP.UTF-8');
+		$error_options = array();
+
+		if ( ! isset( $rakuten['ftp_user'] ) || ! $rakuten['ftp_user'] ) {
+			$error_options = array( ...$error_options, '楽天セットアップ > FTPユーザー' );
+		}
+		if ( ! isset( $rakuten['ftp_pass'] ) || ! $rakuten['ftp_pass'] ) {
+			$error_options = array( ...$error_options, '楽天セットアップ > FTPパスワード' );
+		}
+		if ( $error_options ) {
+			// エラー出力して終了
+			echo '楽天セットアップが完了していません。';
+			exit();
+			die();
+		}
+		$ftp_user = $rakuten['ftp_user'];
+		$ftp_pass = $rakuten['ftp_pass'];
+
+		extract( $_POST );
+
+		if ( 'ftp_img' === $judge ) {
+			setlocale( LC_ALL, 'ja_JP.UTF-8' );
+			extract( $_FILES[ $judge ] ); // $name $type $tmp_name
+			if ( ! empty( $tmp_name[0] ) ) {
+				// テンポラリディレクトリ作成
+				$tmp = wp_tempnam( __CLASS__, dirname( __DIR__ ) . '/' );
+				unlink( $tmp );
+				mkdir( $tmp );
+				$img_dir = rtrim( $rakuten['img_dir'], '/' ) . '/';
+				// GOLD（ne.jp）とキャビネット（co.jp）を判定して接続先を変更
+				$server = preg_match( '/ne\.jp/', $img_dir ) ? 'ftp_server' : 'upload_server';
+				$port = "{$server}_port";
+				$set_server = $rakuten[$server];
+				$set_port = $rakuten[$port];
+				$conn_id = ftp_connect( $set_server, $set_port ); // 可変変数
+				$login = ftp_login( $conn_id, $ftp_user, $ftp_pass );
+				// ログインできない場合は末尾を２に変更
+				if ( ! $login ) {
+					$login = ftp_login( $conn_id, $ftp_user, substr( $ftp_pass, 0, 7 ) . '2' );
+				}
+				if ( $login ) {
+					ftp_pasv( $conn_id, true );
+					foreach ( $tmp_name as $k => $file ) {
+						// 画像圧縮処理
+						$quality = isset( $quality ) ? $quality : 50;
+						move_uploaded_file( $file, "{$tmp}/{$name[$k]}" );
+						exec( "mogrify -quality {$quality} {$tmp}/{$name[$k]}" );
+
+						// jpg以外処理中止
+						if ( strpos( $name[$k], '.jpg' ) !== false ) {
+
+							// GOLDの場合
+							if ( preg_match( '/ne\.jp/', $img_dir ) ) {
+								preg_match( '/https:\/\/([^\/]+\/){3}/u', $img_dir, $match );
+								$remote_file = str_replace( $match[0], '', $img_dir ) . $name[$k];
+								echo $remote_file;
+								if ( ftp_put( $conn_id, $remote_file, "{$tmp}/{$name[$k]}", FTP_ASCII ) ) {
+									echo "楽天GOLDに転送成功 $name[$k]\n";
+								} else {
+									echo "楽天GOLDに転送失敗 $name[$k]\n";
+								}
+							}
+							// キャビネットの場合
+							else {
+								// $img_dir からキャビネットのディレクトリ構造を作成
+								$remote_dir = preg_replace( '/^.*cabinet/', 'cabinet/images', $img_dir );
+								preg_match( '/^([a-z]{2,3})[0-9]{2,3}[-]*[0-9]*\.jpg/', $name[$k], $m );
+								if ( $m[1] ) { // 商品画像の場合
+									if ( ftp_mkdir( $conn_id, $remote_dir ) ) {
+										echo "{$remote_dir}を作成\n";
+									};
+									$remote_dir .= $m[1];
+									if ( ftp_mkdir( $conn_id, $remote_dir ) ) {
+										echo "{$remote_dir}を作成\n";
+									};
+									$remote_file = "{$remote_dir}/{$name[$k]}";
+									if ( ftp_put( $conn_id, $remote_file, "{$tmp}/{$name[$k]}", FTP_ASCII ) ) {
+										echo "キャビネットに転送成功 $name[$k]\n";
+									} else {
+										echo "キャビネットに転送失敗 $name[$k]\n";
+									}
+								} else {
+									echo 'ファイルが違います';
+								}
+							}
+						} else {
+							echo 'ファイルが違います！';
+						}
+					}
+					ftp_close( $conn_id );
+				} else {
+					echo 'パスワードが違います';
+					}
+				exec( "rm -Rf {$tmp}" );
+			} else {
+				echo 'ファイルをセットしてください。';
+			}
+		}
+		// 楽天Uにデータ転送
+		elseif ( 'ftp_file' === $judge ) {
+			setlocale( LC_ALL, 'ja_JP.UTF-8' );
+			extract( $_FILES[$judge] );
+			if ( ! empty( $tmp_name[0] ) ) {
+				$upload_server = $rakuten['upload_server'];
+				$upload_server_port = $rakuten['upload_server_port'];
+				$conn_id = ftp_connect( $upload_server, $upload_server_port );
+				$login = ftp_login( $conn_id, $ftp_user, $ftp_pass );
+				if ( ! $login ) {
+					$login = ftp_login( $conn_id, $ftp_user, substr( $ftp_pass, 0, 7 ) . '2' );
+				} // ログインできない場合は末尾を２に変更
+				if ( $login ) {
+					ftp_pasv( $conn_id, true );
+					foreach ( $tmp_name as $k => $file ) {
+						if ( strpos( $name[$k], '.csv' ) !== false ) {
+							$remote_file = 'ritem/batch/' . $name[$k];
+							if ( ftp_put( $conn_id, $remote_file, $file, FTP_ASCII ) ) {
+								echo "転送成功 $name[$k]\n";
+							} else {
+								echo "転送失敗 $name[$k]\n";
+							}
+						} else {
+							echo 'ファイルが違います！';
+						}
+					}
+					ftp_close( $conn_id );
+				} else {
+					echo 'パスワードが違います';
+				}
+			} else {
+				echo 'ファイルをセットしてください。';
+			}
+		}
+		exit();
+		die();
+	}
+
+	/**
+	 * エラーログ（超突貫）
+	 */
+	public function error_log() {
 		global $n2;
 		WP_Filesystem();
 		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-ftpsockets.php';
 		$opt = array(
-			'hostname' => $n2->rakuten['upload_server'],
-			'username' => $n2->rakuten['ftp_user'],
-			'password' => $n2->rakuten['ftp_pass'],
+			'hostname' => $n2->portal_setting['楽天']['upload_server'],
+			'username' => $n2->portal_setting['楽天']['ftp_user'],
+			'password' => $n2->portal_setting['楽天']['ftp_pass'],
 		);
 		$ftp = new WP_Filesystem_ftpsockets( $opt );
 		$ftp->connect();
-
-		echo '<pre>';print_r($ftp->dirlist('ritem/logs'));echo '</pre>';
-		$contents = $ftp->get_contents('ritem/logs/item-cat.csv');
-		$contents = mb_convert_encoding( $contents, 'utf-8', 'sjis' );
-		echo '<pre>';print_r($contents);echo '</pre>';
+		if ( ! empty( $ftp->errors->errors ) ) {
+			$opt['password'] = rtrim( $opt['password'], 1 ) .'2';
+			$ftp             = new WP_Filesystem_ftpsockets( $opt );
+			$ftp->connect();
+		}
+		$logs = $ftp->dirlist( 'ritem/logs' );
+		$logs = array_reverse( $logs );
+		if ( empty( $logs ) ) {
+			echo "エラーログはありません。";
+			exit;
+		}
+		?>
+		<h3>エラーログ</h3>
+		<table class="widefat striped" style="margin: 2em 0;">
+		<?php
+		foreach ( $logs as $log ) :
+			$contents = $ftp->get_contents( "ritem/logs/{$log['name']}" );
+			$contents = htmlspecialchars( mb_convert_encoding( $contents, 'utf-8', 'sjis' ) );
+			?>
+			<tr>
+				<td><?php echo "{$log['year']} {$log['month']} {$log['day']}"; ?></td>
+				<td>
+					<button type="button" popovertarget="<?php echo $log['name']; ?>" class="button button-primary">エラー内容を見る</button>
+					<div popover="auto" id="<?php echo $log['name']; ?>" style="width: 80%; max-height: 80%; overflow-y: scroll;"><pre><?php echo $contents; ?></pre></div>
+				</td>
+				<td><?php echo "ritem/logs/{$log['name']}"; ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</table>
+		<?php
 	}
 
 	/**
@@ -83,9 +284,9 @@ class N2_Rakuten_FTP {
 		WP_Filesystem();
 		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-ftpsockets.php';
 		$opt = array(
-			'hostname' => $n2->rakuten['upload_server'],
-			'username' => $n2->rakuten['ftp_user'],
-			'password' => $n2->rakuten['ftp_pass'],
+			'hostname' => $n2->portal_setting['楽天']['upload_server'],
+			'username' => $n2->portal_setting['楽天']['ftp_user'],
+			'password' => $n2->portal_setting['楽天']['ftp_pass'],
 		);
 		$ftp = new WP_Filesystem_ftpsockets( $opt );
 		$ftp->connect();
@@ -130,8 +331,4 @@ class N2_Rakuten_FTP {
 		exit;
 	}
 
-	private function ftp_upload( $ftp ) {
-		echo 'ftp_download';
-		exit;
-	}
 }
