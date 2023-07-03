93c11ebd59c6b67f73ce36b44c87626a27800a65
ryota-nakano <rnakanor@gmail.com>
2023-07-03 18:06:11 +0900

    :zap: 土岐・多治見の返礼品の出品基準チェックに対応

--- template/settings/n2.php
+++ template/settings/n2.php
@@ -70,4 +70,13 @@ $settings = array(
 			</label>
 		</td>
 	</tr>
+	<tr>
+		<th>返礼品の出品基準チェック</th>
+		<td>
+			<input type="hidden" name="n2_settings[N2][返礼品の出品基準チェック]">
+			<label>
+				<input type="checkbox" name="n2_settings[N2][返礼品の出品基準チェック]" value="1" <?php checked( $n2->settings['N2']['返礼品の出品基準チェック'] ); ?>> 返礼品の出品基準チェックする
+			</label>
+		</td>
+	</tr>
 </table>
