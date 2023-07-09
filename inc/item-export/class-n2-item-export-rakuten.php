cc9c258a8007768451e50787d234299536f58ef7
yamasaki-hiroyoshi <hiroyoshi.yamasaki@steamship.co.jp>
2023-07-10 08:15:13 +0900

    :sparckes:PC商品説明文にもやきもの情報表示

--- inc/item-export/class-n2-item-export-rakuten.php
+++ inc/item-export/class-n2-item-export-rakuten.php
@@ -453,17 +453,25 @@ class N2_Item_Export_Rakuten extends N2_Item_Export_Base {
 			<?php echo apply_filters( 'n2_item_export_rakuten_porcelain_text', '', $n2values['id'], '対応機器' ); ?>
 			<?php if ( $n2values['原料原産地'] ) : ?>
 				<br><br>【原料原産地】<br>
-				<?php echo nl2br( $n2values['原料原産地'] ); ?>
+				<?php echo nl2br( $n2values['原料原産地'] ); ?><br>
 			<?php endif; ?>
 			<?php if ( $n2values['加工地'] ) : ?>
 				<br><br>【加工地】<br>
 				<?php echo nl2br( $n2values['加工地'] ); ?><br>
 			<?php endif; ?>
+			<?php if ( $n2values['電子レンジ対応'] || $n2values['オーブン対応'] || $n2values['食洗機対応'] ) : ?>
+				<br><br>【対応機器】<br>
+				<?php echo '電子レンジ' . $n2values['電子レンジ対応'] . ' / オーブン' . $n2values['オーブン対応'] . ' / 食器洗浄機' . $n2values['食洗機対応']; ?><br>
+			<?php endif; ?>
+			<?php if ( $n2values['対応機器備考'] ) : ?>
+				<br><br>【対応機器備考】<br>
+				<?php echo nl2br( $n2values['対応機器備考'] ); ?><br>
+			<?php endif; ?>
 			<?php if ( $n2values['検索キーワード'] ) : ?>
-				<br><br><?php echo nl2br( $n2values['検索キーワード'] ); ?>
+				<br><br><?php echo nl2br( $n2values['検索キーワード'] ); ?><br>
 			<?php endif; ?>
 			<?php if ( $n2values['楽天SPAカテゴリー'] ) : ?>
-				<br><br><?php echo nl2br( $n2values['楽天SPAカテゴリー'] ); ?>
+				<br><br><?php echo nl2br( $n2values['楽天SPAカテゴリー'] ); ?><br>
 			<?php endif; ?>
 			<?php
 		};
