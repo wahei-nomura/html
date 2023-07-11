6a243b6bfe9585b93c9f7de798277e1466ca2f64
ryota-nakano <rnakanor@gmail.com>
2023-07-11 18:36:38 +0900

    :cracker: 画像ダウンロードをrequest_multiple対応（オレオレ証明書も）

--- inc/class-n2-img-download.php
+++ inc/class-n2-img-download.php
@@ -10,6 +10,7 @@ if ( class_exists( 'N2_Img_Download' ) ) {
 	return;
 }
 require_once ABSPATH . '/wp-admin/includes/file.php';
+use WpOrg\Requests\HookManager;
 /**
  * Img_Download
  */
@@ -44,6 +45,7 @@ class N2_Img_Download {
 	 * 投稿IDからダウンロード
 	 */
 	public function download_images_by_id() {
+		global $n2;
 		$params = $_GET;
 		// $_POSTを$paramsで上書き
 		if ( wp_verify_nonce( $_POST['n2nonce'] ?? '', 'n2nonce' ) ) {
@@ -56,11 +58,13 @@ class N2_Img_Download {
 		WP_Filesystem();
 		global $wp_filesystem;
 		// localhostでのwp_remote_getに必須
-		add_filter( 'https_ssl_verify', '__return_false' );
-		$ids     = explode( ',', $params['ids'] );
-		$tmp_uri = stream_get_meta_data( tmpfile() )['uri'];
-		$zip     = new ZipArchive();
+		$ids      = explode( ',', $params['ids'] );
+		$tmp_uri  = stream_get_meta_data( tmpfile() )['uri'];
+		$zip      = new ZipArchive();
+		$requests = array();// urlリスト
+		$info     = array();// 画像情報
 		$zip->open( $tmp_uri, ZipArchive::CREATE );
+
 		foreach ( $ids as $id ) {
 			$img_file_name = get_post_meta( $id, '返礼品コード', true );
 			$dirname       = $img_file_name ?: get_the_title( $id );
@@ -68,7 +72,47 @@ class N2_Img_Download {
 			foreach ( get_post_meta( $id, '商品画像', true ) as $i => $img ) {
 				$index     = $i + 1;
 				$extension = pathinfo( $img['url'] )['extension'];
-				$zip->addFromString( "{$dirname}/{$filename}-{$index}.{$extension}", wp_remote_get( $img['url'] )['body'] );
+				// オレオレ証明書も許可する
+				$hooks = new Requests_Hooks();
+				$hooks->register(
+					'curl.before_multi_add',
+					function ( $ch ) {
+						curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+					}
+				);
+				// 配列生成
+				$requests[] = array(
+					'url' => $img['url'],
+					'options' => array(
+						'hooks' => $hooks,
+					),
+				);
+				$info[ $img['url'] ] = array(
+					'dirname'     => $dirname,
+					'filename'    => "{$filename}-{$index}.{$extension}",
+					'description' => $img['description'] ?? '',
+				);
+			}
+		}
+		$description = array();
+		foreach ( Requests::request_multiple( $requests, array( 'timeout' => 100 ) ) as $v ) {
+			$v->info = $info[ $v->url ];
+			// 説明を追加
+			if ( ! empty( $info[ $v->url ]['description'] ) ) {
+				$description[ $v->info['dirname'] ][] = array(
+					'dirname'     => $v->info['dirname'],
+					'filename'    => $v->info['filename'],
+					'description' => $v->info['description'],
+				);
+			}
+			$zip->addFromString( "{$v->info['dirname']}/{$v->info['filename']}", $v->body );
+		}
+		// 説明.txt生成
+		if ( ! empty( $description ) ) {
+			foreach ( $description as $dirname => $desc ) {
+				array_multisort( array_column( $desc, 'filename' ), SORT_ASC, $desc );
+				$desc = implode( PHP_EOL, array_map( fn( $v ) => implode( "\t", $v ), $desc ) );
+				$zip->addFromString( "{$dirname}/説明.txt", $desc );
 			}
 		}
 		// 単品か複数かで名前と構造を変更
