fe4735113834926bd86f309bb6a53ae0915ba491
yamasaki-hiroyoshi <hiroyoshi.yamasaki@steamship.co.jp>
2023-07-11 15:33:48 +0900

    itemとselectは出せるようになった(複数にも対応した)

--- inc/class-n2-change-sku-firstaid.php
+++ inc/class-n2-change-sku-firstaid.php
@@ -31,26 +31,21 @@ class N2_Change_Sku_Firstaid {
 	 * @return void
 	 */
 	public function change_sku_firstaid() {
-		header( 'Content-Type: application/json; charset=utf-8' );
-		$files                          = $_FILES['item_files'];
-		$new_item_column                = '商品管理番号（商品URL）,商品番号,商品名,倉庫指定,ジャンルID,非製品属性タグID,キャッチコピー,PC用商品説明文,スマートフォン用商品説明文,PC用販売説明文,商品画像タイプ1,商品画像パス1,商品画像名（ALT）1,商品画像タイプ2,商品画像パス2,商品画像名（ALT）2,商品画像タイプ3,商品画像パス3,商品画像名（ALT）3,商品画像タイプ4,商品画像パス4,商品画像名（ALT）4,商品画像タイプ5,商品画像パス5,商品画像名（ALT）5,商品画像タイプ6,商品画像パス6,商品画像名（ALT）6,商品画像タイプ7,商品画像パス7,商品画像名（ALT）7,商品画像タイプ8,商品画像パス8,商品画像名（ALT）8,商品画像タイプ9,商品画像パス9,商品画像名（ALT）9,商品画像タイプ10,商品画像パス10,商品画像名（ALT）10,商品画像タイプ11,商品画像パス11,商品画像名（ALT）11,商品画像タイプ12,商品画像パス12,商品画像名（ALT）12,商品画像タイプ13,商品画像パス13,商品画像名（ALT）13,商品画像タイプ14,商品画像パス14,商品画像名（ALT）14,商品画像タイプ15,商品画像パス15,商品画像名（ALT）15,商品画像タイプ16,商品画像パス16,商品画像名（ALT）16,商品画像タイプ17,商品画像パス17,商品画像名（ALT）17,商品画像タイプ18,商品画像パス18,商品画像名（ALT）18,商品画像タイプ19,商品画像パス19,商品画像名（ALT）19,商品画像タイプ20,商品画像パス20,商品画像名（ALT）20,バリエーション項目キー定義,バリエーション項目名定義,バリエーション1選択肢定義,バリエーション2選択肢定義,バリエーション3選択肢定義,選択肢タイプ,商品オプション項目名,商品オプション選択肢1,商品オプション選択肢2,商品オプション選択肢3,商品オプション選択必須,SKU管理番号,販売価格,再入荷お知らせボタン,のし対応,在庫数,在庫あり時納期管理番号,送料,カタログIDなしの理由';
-		$new_item_column_array          = explode( ',', $new_item_column );
-		$output_column[ $column_count ] = $new_item_column;
-		$column_count++;
+		$files = $_FILES['item_files'];
 
+		// csvの読み込み&配列格納
 		for ( $i = 0; $i < count( $files['name'] ); $i++ ) {
-			print_r($i);
-			$filename      = $files['name'][ $i ];
-			$filetmp       = $files['tmp_name'][ $i ];
-			$filesize      = $files['size'][ $i ];
-			$fileerror     = $files['error'][ $i ];
-			$result_item   = preg_match( '/.*item.*/', $filename );
-			$result_select = preg_match( '/.*select.*/', $filename );
-			$fn            = fopen( $filetmp, 'r' );
-			$countloop     = 0;
-			$csv_header_array = [];
+			$filename         = $files['name'][ $i ];
+			$filetmp          = $files['tmp_name'][ $i ];
+			$filesize         = $files['size'][ $i ];
+			$fileerror        = $files['error'][ $i ];
+			$result_item      = preg_match( '/.*item.*/', $filename );
+			$result_select    = preg_match( '/.*select.*/', $filename );
+			$fn               = fopen( $filetmp, 'r' );
+			$countloop        = 0;
+			$csv_header_array = array();
 			while ( ( $arr = fgetcsv( $fn ) ) != false ) {
-				${'csv_array_' . $countloop} = [];
+				${'csv_array_' . $countloop} = array();
 				foreach ( $arr as $key => $val ) {
 					if ( 0 === $countloop ) {
 						$csv_header_array[] = mb_convert_encoding( $val, 'UTF-8', 'SJIS' );
@@ -63,6 +58,7 @@ class N2_Change_Sku_Firstaid {
 			if ( $result_item ) {
 				// item.csvの要素を配列($csv_item_array)に格納
 				$csv_item_array = array();
+				$csv_item_count = 0;
 				for ( $j = 0; $j < $countloop; ++$j ) {
 					if ( 0 == $j ) {
 						$csv_item_array[] = $csv_header_array;
@@ -70,143 +66,118 @@ class N2_Change_Sku_Firstaid {
 						$csv_item_array[] = ${'csv_array_' . $j};
 					}
 				}
+				$csv_item_count = $countloop;
 			}
 			if ( $result_select ) {
 				// select.csvの要素を配列($csv_select_array)に格納
 				$csv_select_array = array();
-				for ( $j = 0; $j < $countloop; ++$j ) {
-					if ( 0 == $j ) {
-						$csv_select_array[] = $csv_header_array;
-					} else {
-						$csv_select_array[] = ${'csv_array_' . $j};
+				$csv_select_count = 0;
+				for ( $j = 1; $j < $countloop; ++$j ) {
+					if ( ${'csv_array_' . $j}[3] !== $csv_select_array[ $csv_select_count ][1] ) {
+						$csv_select_count++;
+						$csv_select_array[ $csv_select_count ][0] = 's';
+						$csv_select_array[ $csv_select_count ][1] = ${'csv_array_' . $j}[3];
 					}
+					$csv_select_array[ $csv_select_count ][] = ${'csv_array_' . $j}[4];
 				}
 			}
 		}
-		// 各種設定読み込み
-		global $n2;
-		$column_count = 0;
-		$rakuten      = $n2->portal_setting['楽天'];
-		// setlocale(LC_ALL, 'ja_JP.UTF-8');
-		$error_options = array();
-
-		if ( ! isset( $rakuten['select'] ) || ! $rakuten['select'] ) {
-			$error_options = array( ...$error_options, '楽天セットアップ > 項目選択肢' );
-		}
-		if ( $error_options ) {
-			// エラー出力して終了
-			echo '項目選択肢の入力が完了していません。';
-			exit();
-			die();
-		}
-		$select_items = $rakuten['項目選択肢'];
-
+		$this->output_csv( $csv_item_array, $csv_select_array );
+	}
+	public function output_csv( $csv_item_array, $csv_select_array ) {
+		header( 'Content-Type: application/json; charset=utf-8' );
 		setlocale( LC_ALL, 'ja_JP.UTF-8' );
-		extract( $_FILES['item_files'] );
-		if ( ! empty( $tmp_name[0] ) ) {
-			$new_item_column                = '商品管理番号（商品URL）,商品番号,商品名,倉庫指定,ジャンルID,非製品属性タグID,キャッチコピー,PC用商品説明文,スマートフォン用商品説明文,PC用販売説明文,商品画像タイプ1,商品画像パス1,商品画像名（ALT）1,商品画像タイプ2,商品画像パス2,商品画像名（ALT）2,商品画像タイプ3,商品画像パス3,商品画像名（ALT）3,商品画像タイプ4,商品画像パス4,商品画像名（ALT）4,商品画像タイプ5,商品画像パス5,商品画像名（ALT）5,商品画像タイプ6,商品画像パス6,商品画像名（ALT）6,商品画像タイプ7,商品画像パス7,商品画像名（ALT）7,商品画像タイプ8,商品画像パス8,商品画像名（ALT）8,商品画像タイプ9,商品画像パス9,商品画像名（ALT）9,商品画像タイプ10,商品画像パス10,商品画像名（ALT）10,商品画像タイプ11,商品画像パス11,商品画像名（ALT）11,商品画像タイプ12,商品画像パス12,商品画像名（ALT）12,商品画像タイプ13,商品画像パス13,商品画像名（ALT）13,商品画像タイプ14,商品画像パス14,商品画像名（ALT）14,商品画像タイプ15,商品画像パス15,商品画像名（ALT）15,商品画像タイプ16,商品画像パス16,商品画像名（ALT）16,商品画像タイプ17,商品画像パス17,商品画像名（ALT）17,商品画像タイプ18,商品画像パス18,商品画像名（ALT）18,商品画像タイプ19,商品画像パス19,商品画像名（ALT）19,商品画像タイプ20,商品画像パス20,商品画像名（ALT）20,バリエーション項目キー定義,バリエーション項目名定義,バリエーション1選択肢定義,バリエーション2選択肢定義,バリエーション3選択肢定義,選択肢タイプ,商品オプション項目名,商品オプション選択肢1,商品オプション選択肢2,商品オプション選択肢3,商品オプション選択必須,SKU管理番号,販売価格,再入荷お知らせボタン,のし対応,在庫数,在庫あり時納期管理番号,送料,カタログIDなしの理由';
-			$output_column[ $column_count ] = $new_item_column;
-			$column_count++;
-			$new_item_column_array = explode( ',', $new_item_column );
-			// 旧項目：コントロールカラム,商品管理番号（商品URL）,商品番号,全商品ディレクトリID,タグID,PC用キャッチコピー,モバイル用キャッチコピー,商品名,販売価格,送料,のし対応,PC用商品説明文,スマートフォン用商品説明文,PC用販売説明文,商品画像URL,在庫タイプ,在庫数,カタログID,カタログIDなしの理由
-			$fn        = fopen( $tmp_name[0], 'r' );
-			$countloop = 0;
-			while ( ( $arr = fgetcsv( $fn ) ) != false ) {
-				foreach ( $arr as $key => $val ) {
-					if ( 0 === $countloop ) {
-						$arr0[] = mb_convert_encoding( $val, 'UTF-8', 'SJIS' );
-					}
-					if ( 1 === $countloop ) {
-						$arr1[] = str_replace( '"', '""', mb_convert_encoding( $val, 'UTF-8', 'SJIS' ) ?? '' );
-					}
-				}
-				$countloop++;
+		$new_item_column = '商品管理番号（商品URL）,商品番号,商品名,倉庫指定,ジャンルID,非製品属性タグID,キャッチコピー,PC用商品説明文,スマートフォン用商品説明文,PC用販売説明文,商品画像タイプ1,商品画像パス1,商品画像名（ALT）1,商品画像タイプ2,商品画像パス2,商品画像名（ALT）2,商品画像タイプ3,商品画像パス3,商品画像名（ALT）3,商品画像タイプ4,商品画像パス4,商品画像名（ALT）4,商品画像タイプ5,商品画像パス5,商品画像名（ALT）5,商品画像タイプ6,商品画像パス6,商品画像名（ALT）6,商品画像タイプ7,商品画像パス7,商品画像名（ALT）7,商品画像タイプ8,商品画像パス8,商品画像名（ALT）8,商品画像タイプ9,商品画像パス9,商品画像名（ALT）9,商品画像タイプ10,商品画像パス10,商品画像名（ALT）10,商品画像タイプ11,商品画像パス11,商品画像名（ALT）11,商品画像タイプ12,商品画像パス12,商品画像名（ALT）12,商品画像タイプ13,商品画像パス13,商品画像名（ALT）13,商品画像タイプ14,商品画像パス14,商品画像名（ALT）14,商品画像タイプ15,商品画像パス15,商品画像名（ALT）15,商品画像タイプ16,商品画像パス16,商品画像名（ALT）16,商品画像タイプ17,商品画像パス17,商品画像名（ALT）17,商品画像タイプ18,商品画像パス18,商品画像名（ALT）18,商品画像タイプ19,商品画像パス19,商品画像名（ALT）19,商品画像タイプ20,商品画像パス20,商品画像名（ALT）20,バリエーション項目キー定義,バリエーション項目名定義,バリエーション1選択肢定義,バリエーション2選択肢定義,バリエーション3選択肢定義,選択肢タイプ,商品オプション項目名,商品オプション選択肢1,商品オプション選択肢2,商品オプション選択肢3,商品オプション選択必須,SKU管理番号,販売価格,再入荷お知らせボタン,のし対応,在庫数,在庫あり時納期管理番号,送料,カタログIDなしの理由';
+		$output_data     = $new_item_column . "\n";
+		// １行目：新ヘッダー
+		$new_item_column_array = explode( ',', $new_item_column );
+		// selectの出力データを作る(商品管理番号はまだ入れない)
+		$output_select = '';
+		$select_no     = array_search( '選択肢タイプ', $new_item_column_array );
+		foreach ( $csv_select_array as $csv_select ) {
+			$select_array            = array();
+			$new_select_value_column = '';
+			for ( $i = 0; $i < $select_no; $i++ ) {
+				$select_array[ $i ] = '';
 			}
-			$item_array = array_combine( $arr0, $arr1 );
-			// 商品画像URLを分割
-			if ( isset( $item_array['商品画像URL'] ) && '' !== $item_array['商品画像URL'] ) {
-				$picture_array = explode( ' ', $item_array['商品画像URL'] );
-				foreach ( $picture_array as $picture_key => $picture_val ) {
-					$picture_name         = explode( '/', $picture_val );
-					$picture_last_names[] = $picture_name[ array_key_last( $picture_name ) - 2 ] . '/' . $picture_name[ array_key_last( $picture_name ) - 1 ] . '/' . $picture_name[ array_key_last( $picture_name ) ];
-				}
-				$picture_count = count( $picture_last_names );
+			$select_array[0] = '商品管理番号(仮)';
+			foreach ( $csv_select as $select_key => $value ) {
+				$select_array[ $select_no + $select_key + 1 ] = $value;
 			}
-			foreach ( $new_item_column_array as $item_key => $item_val ) {
-				if ( isset( $item_array[ $item_val ] ) ) {
-					$new_item_array[ $item_key ] = $item_array[ $item_val ];
-				} elseif ( $item_val === '非製品属性タグID' ) { // タグIDは/(スラッシュ)から|(パイプ)に変換
-					$new_tag_id_array            = explode( '/', $item_array['タグID'] );
-					$new_tag_id                  = implode( '|', $new_tag_id_array );
-					$new_item_array[ $item_key ] = $new_tag_id;
-				} elseif ( $item_val === 'カタログIDなしの理由' ) { // 5で固定
-					$new_item_array[ $item_key ] = '5';
-				} elseif ( $item_val === 'キャッチコピー' ) { // モバイル用を入れる
-					$new_item_array[ $item_key ] = $item_array['モバイル用キャッチコピー'];
-				} elseif ( $item_val === 'SKU管理番号' ) {
-					$sku_key                     = $item_key; // SKUスタートの番号を取っておく
-					$new_item_array[ $item_key ] = $item_array['商品管理番号（商品URL）'];
+			foreach ( $select_array as $select_value_key => $select_value ) {
+				if ( $select_value_key !== 0 ) {
+					$new_select_value_column .= ',';
+				}
+				if ( '' !== $select_value ) {
+					$new_select_value_column .= '"' . $select_value . '"';
 				} else {
-					$new_item_array[ $item_key ] = '';
+					$new_select_value_column .= $select_value;
 				}
 			}
-			for ( $i = 0; $i < $picture_count; $i++ ) {
-				$picture_type_no                   = '商品画像タイプ' . $i + 1;
-				$picture_path_no                   = '商品画像パス' . $i + 1;
-				$picture_no                        = array_search( $picture_type_no, $new_item_column_array );
-				$new_item_array[ $picture_no ]     = 'CABINET';
-				$new_item_array[ $picture_no + 1 ] = $picture_last_names[ $i ];
-			}
-			foreach ( $new_item_array as $new_item_key => $new_item ) {
-				if ( $new_item_key !== 0 ) {
-					$new_item_value_column .= ',';
-				}
-				if ( '' !== $new_item ) {
-					$new_item_value_column .= '"' . $new_item . '"';
-				} else {
-					$new_item_value_column .= $new_item;
-				}
-			}
-			$output_column[ $column_count ] = $new_item_value_column;
-			$column_count++;
-			// 項目選択肢
-			$selects      = $n2->portal_setting['楽天']['select'];
-			$selects      = str_replace( array( "\r\n", "\r" ), "\n", $selects );// 改行コード統一
-			$selects      = preg_split( '/\n{2,}/', $selects );// 連続改行で分ける
-			$select_count = count( $selects );
-			if ( $select_count > 0 ) {
-				$select_no = array_search( '選択肢タイプ', $new_item_column_array );
-				foreach ( $selects as $select ) {
-					$select_array = array();
-					for ( $i = 0; $i < $select_no; $i++ ) {
-						$select_array[ $i ] = '';
-					}
-					$select                     = explode( "\n", $select );
-					$select_array[0]            = $item_array['商品管理番号（商品URL）'];
-					$select_array[ $select_no ] = 's';
-					$new_select_value_column    = '';
-					foreach ( $select as $select_key => $value ) {
-						$select_array[ $select_no + $select_key + 1 ] = $value;
-					}
-					foreach ( $select_array as $select_value_key => $select_value ) {
-						if ( $select_value_key !== 0 ) {
-							$new_select_value_column .= ',';
-						}
-						if ( '' !== $select_value ) {
-							$new_select_value_column .= '"' . $select_value . '"';
-						} else {
-							$new_select_value_column .= $select_value;
-						}
-					}
-					$output_column[ $column_count ] = $new_select_value_column;
-					$column_count++;
-				}
-			}
-			foreach ( $output_column as $output ) {
-				echo $output . "\n";
-			}
-			exit();
-			die();
+			$output_select .= $new_select_value_column . "\n";
 		}
+		// itemの出力データを作る
+		$picture_no = array_search( '商品画像URL', $csv_item_array[0] );
+		foreach ( $csv_item_array as $item_key => $csv_item ) {
+			$picture_last_names = array();
+			if ( $item_key !== 0 ) { // headerは除外
+				// 商品画像URLを分割
+				if ( isset( $csv_item[ $picture_no ] ) && '' !== $csv_item[ $picture_no ] ) {
+					$picture_array = explode( ' ', $csv_item[ $picture_no ] );
+					foreach ( $picture_array as $picture_key => $picture_val ) {
+						$picture_name         = explode( '/', $picture_val );
+						$picture_last_names[] = $picture_name[ array_key_last( $picture_name ) - 2 ] . '/' . $picture_name[ array_key_last( $picture_name ) - 1 ] . '/' . $picture_name[ array_key_last( $picture_name ) ];
+					}
+					$picture_count = count( $picture_last_names );
+				}
+				foreach ( $new_item_column_array as $new_item_key => $item_val ) {
+					if ( array_search( $item_val, $csv_item_array[0] ) ) {
+						$item_val_no = array_search( $item_val, $csv_item_array[0] );
+						if ( $item_val === '商品管理番号（商品URL）' ) {
+							$code_value = $csv_item[ $item_val_no ];
+						}
+						$new_item_array[ $new_item_key ] = $csv_item[ $item_val_no ];
+					} elseif ( $item_val === '非製品属性タグID' ) { // タグIDは/(スラッシュ)から|(パイプ)に変換
+						$tag_id_no                       = array_search( 'タグID', $csv_item_array[0] );
+						$new_tag_id_array                = explode( '/', $csv_item[ $tag_id_no ] );
+						$new_tag_id                      = implode( '|', $new_tag_id_array );
+						$new_item_array[ $new_item_key ] = $new_tag_id;
+					} elseif ( $item_val === 'カタログIDなしの理由' ) { // 5で固定
+						$new_item_array[ $new_item_key ] = '5';
+					} elseif ( $item_val === 'キャッチコピー' ) { // モバイル用を入れる
+						$catchcopy_no                    = array_search( 'モバイル用キャッチコピー', $csv_item_array[0] );
+						$new_item_array[ $new_item_key ] = $item_array[ $catchcopy_no ];
+					} elseif ( $item_val === 'SKU管理番号' ) {
+						$sku_key                         = $new_item_key; // SKUスタートの番号を取っておく
+						$new_item_array[ $new_item_key ] = '';
+					} else {
+						$new_item_array[ $new_item_key ] = '';
+					}
+				}
+				for ( $i = 0; $i < $picture_count; $i++ ) {
+					$picture_type_no                       = '商品画像タイプ' . $i + 1;
+					$picture_path_no                       = '商品画像パス' . $i + 1;
+					$new_picture_no                        = array_search( $picture_type_no, $new_item_column_array );
+					$new_item_array[ $new_picture_no ]     = 'CABINET';
+					$new_item_array[ $new_picture_no + 1 ] = $picture_last_names[ $i ];
+				}
+				foreach ( $new_item_array as $new_item_key => $new_item ) {
+					if ( $new_item_key !== 0 ) {
+						$output_data .= ',';
+					}
+					if ( '' !== $new_item ) {
+						$output_data .= '"' . $new_item . '"';
+					} else {
+						$output_data .= $new_item;
+					}
+				}
+				$output_data       .= "\n";
+				$new_output_select .= str_replace( '商品管理番号(仮)', $code_value, $output_select );
+				$output_data       .= $new_output_select;
+			}
+		}
+		print_r( $output_data );
+		exit();
+		die();
 	}
 }
--- template/change-sku.php
+++ template/change-sku.php
@@ -12,39 +12,6 @@
 <style>
 	#n2-change-author-modal {
 		padding: 24px;
-		overflow: scroll;
-	}
-	#n2-change-author-modal label {
-		display: block;
-		font-size: 18px;
-		font-weight: bold;
-		margin-bottom: 8px;
-	}
-	#n2-change-author-modal .form-block {
-		position: relative;
-		margin: 16px 0;
-		padding: 16px;
-		background-color: lightgray;
-	}
-	#n2-change-author-modal .form-block::after {
-		content: '';
-		position: absolute;
-		bottom: -8px;
-		left: 0;
-		width: 100%;
-		height: 1px;
-	}
-	#n2-change-author-modal .close-btn {
-		position: absolute;
-		right: 16px;
-		top: 16px;
-		z-index: 1000;
-		cursor: pointer;
-	}
-	#n2-change-author-modal .close-btn span {
-		width: 32px;
-		height: 32px;
-		font-size: 32px;
 	}
 </style>
 
