b6a87a31854678f2c5812754fc329a28f8825818
hirotoki3193 <1stlevi777@gmail.com>
2023-07-12 13:44:01 +0900

    :+1:[fix] 商品属性IDがある時にも対応

--- inc/api/class-n2-rms-navigation-api.php
+++ inc/api/class-n2-rms-navigation-api.php
@@ -21,8 +21,12 @@ class N2_RMS_Navigation_API extends N2_RMS_Base_API {
 	 */
 	public static function genres_attributes_get() {
 		static::check_fatal_error( static::$data['params']['genreId'], 'ジャンルIDが未設定です' );
-		$url  = static::$settings['endpoint'] . '2.0/navigation/genres/' . static::$data['params']['genreId'] . '/attributes/';
-		$data = wp_remote_get( $url, array( 'headers' => static::$data['header'] ) );
-		return $data;
+		$url = static::$settings['endpoint'] . '2.0/navigation/genres/' . static::$data['params']['genreId'] . '/attributes/';
+
+		// 商品属性IDがあれば追加する
+		$url .= static::$data['params']['attributeId'] ?? '';
+
+		$response = wp_remote_get( $url, array( 'headers' => static::$data['header'] ) );
+		return $response;
 	}
 }
