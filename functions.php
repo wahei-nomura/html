b70316b314d473dd6e0651f4c6e0f9fa1925c344
hirotoki3193 <1stlevi777@gmail.com>
2023-07-12 13:40:16 +0900

    :tada:[add] Navigation API 追加

--- functions.php
+++ functions.php
@@ -46,6 +46,7 @@ $incs = array(
 	'api/class-n2-rakuten-items-api',
 	'api/class-n2-rms-base-api',
 	'api/class-n2-rms-cabinet-api',
+	'api/class-n2-rms-navigation-api',
 	'api/class-n2-furusato-choice-items-api',
 	'class-n2-chonbo',
 	'api/class-n2-output-gift-api',
