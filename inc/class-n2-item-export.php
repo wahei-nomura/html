574d30e6d749f17c65964c6439efa883a2159dd5
hirotoki3193 <1stlevi777@gmail.com>
2023-07-13 16:13:56 +0900

    :bug:[fix] item-exportの自動取得を手動に変更

--- inc/class-n2-item-export.php
+++ inc/class-n2-item-export.php
@@ -19,9 +19,22 @@ class N2_Item_Export {
 	 */
 	public function __construct() {
 		// エクスポート関連全読込
-		$path = get_theme_file_path( '/inc/item-export/*.php' );
-		foreach ( glob( $path ) as $filename ) {
-			require_once $filename;
+		// $path = get_theme_file_path( '/inc/item-export/*.php' );
+		// foreach ( glob( $path ) as $filename ) {
+		// 	require_once $filename;
+		// }
+		$path    = '/inc/item-export/';
+		$exports = array(
+			'class-n2-item-export-base.php',
+			'class-n2-item-export-furusato-choice.php',
+			'class-n2-item-export-ledghome.php',
+			'class-n2-item-export-rakuten-item.php',
+			'class-n2-item-export-rakuten-select.php',
+			'class-n2-item-export-rakuten-sku.php',
+			'class-n2-product-list-print.php',
+		);
+		foreach ( $exports as $export ) {
+			require_once get_theme_file_path( $path . $export );
 		}
 	}
 }
