e7850ed0950a26a9bbb19680ddea2b320d86f2a2
hirotoki3193 <1stlevi777@gmail.com>
2023-07-21 11:26:30 +0900

    :bug:[fix] hashchange後にajaxは発火させる

--- src/ts/modules/admin-bar-menu-self-destruct.ts
+++ src/ts/modules/admin-bar-menu-self-destruct.ts
@@ -6,21 +6,26 @@ jQuery(function($){
 		if( !confirm('アカウントを削除します。続けますか？') ){
 			return;
 		}
-		const params = {
-			action : 'n2_user_destruct_self_account',
+		if( !confirm('本当に削除しますか？') ){
+			return;
 		}
-		const urlSearchParam =  new URLSearchParams(params).toString();
-		const data = {
-			id : window['n2'].current_user.ID,
-			n2nonce: location.hash.replace('#',''),
-		}
-		$.ajax({
-			url: window['n2'].ajaxurl + '?' + urlSearchParam,
-			type: 'POST',
-			data: data,
-		}).then(res=>{
-			alert(res);
-			location.reload();
+		window.addEventListener('hashchange',function(){
+			const params = {
+				action : 'n2_user_destruct_self_account',
+			}
+			const urlSearchParam =  new URLSearchParams(params).toString();
+			const data = {
+				id : window['n2'].current_user.ID,
+				n2nonce: location.hash.replace('#',''),
+			}
+			$.ajax({
+				url: window['n2'].ajaxurl + '?' + urlSearchParam,
+				type: 'POST',
+				data: data,
+			}).then(res=>{
+				alert(res);
+				location.reload();
+			})
 		})
 	}
 	$('#wp-admin-bar-destruct-self').on('click',destruct_self_account);
