feab4469f24eff5191a29af6df542f3266346d65
ryota-nakano <rnakanor@gmail.com>
2023-07-24 18:52:49 +0900

    :wrench: 独断と偏見でCSS調整

--- inc/class-n2-setmenu.php
+++ inc/class-n2-setmenu.php
@@ -113,11 +113,13 @@ class N2_Setmenu {
 
 	/**
 	 * faviconを変更する
+	 *
+	 * @param string $url デフォルトURL
 	 */
-	public function change_site_icon() {
+	public function change_site_icon( $url ) {
 		$name = end( explode( '/', get_home_url() ) );
 		$n2_active = get_option( 'n2_settings' )['N2']['稼働中'];
-		return $n2_active ? "https://event.rakuten.co.jp/furusato/_pc/img/area/ico/ico_{$name}.png" : get_theme_file_uri( 'no_neo_neng_logo.svg' );
+		return $n2_active ? "https://event.rakuten.co.jp/furusato/_pc/img/area/ico/ico_{$name}.png" : $url;
 	}
 	/**
 	 * 管理画面左上のロゴ変更
