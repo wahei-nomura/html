3147f9b0838c8400f759aed48300b98154224eb4
ryota-nakano <rnakanor@gmail.com>
2023-07-31 09:49:23 +0900

    :wrench: 項目選択肢の設定名を変更したのに対応していなかったので修正

--- inc/item-export/class-n2-item-export-rakuten-select.php
+++ inc/item-export/class-n2-item-export-rakuten-select.php
@@ -52,7 +52,7 @@ class N2_Item_Export_Rakuten_Select extends N2_Item_Export_Base {
 	 */
 	protected function walk_values( &$val, $index, $n2values ) {
 		global $n2;
-		$selects = $n2->settings['楽天']['select'];
+		$selects = $n2->settings['楽天']['項目選択肢'];
 		$selects = str_replace( array( "\r\n", "\r" ), "\n", $selects );// 改行コード統一
 		$selects = preg_split( '/\n{2,}/', $selects );// 連続改行で分ける
 		foreach ( $selects as $select ) {
