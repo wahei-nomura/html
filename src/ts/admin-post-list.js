2c6a404309c682efb8010c97b83c8437915b6460
ryota-nakano <rnakanor@gmail.com>
2023-08-10 12:11:56 +0900

    :wrench: N2の除数か送料を設定してないと設定画面にリダイレクト

--- src/ts/admin-post-list.js
+++ src/ts/admin-post-list.js
@@ -5,4 +5,8 @@ const n2 = window['n2'];
 jQuery( $ => {
 	save_post_ids($);
 	post_list_tool($);
+	if ( ! n2.settings['寄附金額・送料']['除数'] || ! n2.settings['寄附金額・送料']['送料']['0101'] ) {
+		alert('N2の送料設定が正しく完了していません。設定画面へリダイレクトします。');
+		location.href = './admin.php?page=n2_settings_formula-delivery'
+	}
 });
