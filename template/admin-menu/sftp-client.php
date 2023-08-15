e0a493f3861d1419c33b2f772c6119ff4b1cc8d0
hirotoki3193 <1stlevi777@gmail.com>
2023-08-15 14:14:44 +0900

    :+1:[update] 楽天Cabinetをサブメニューに移動

--- template/admin-menu/sftp-client.php
+++ template/admin-menu/sftp-client.php
@@ -99,7 +99,6 @@ global $n2;
 ?>
 <div id="ss-cabinet" class="container">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<div class="d-flex justify-content-between pb-1">
 		<h2>CABINET</h2>
 		<form id="cabinet-search" class="d-flex col-5 p-1">
