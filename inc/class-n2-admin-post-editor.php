bb5f1626a434508fd0f9776313a519c88a3e7ed4
ryota-nakano <rnakanor@gmail.com>
2023-09-07 11:09:42 +0900

    :wrench: 特定のアカウントでエディタの見た目が揃わない問題を修正したつもり。

--- inc/class-n2-admin-post-editor.php
+++ inc/class-n2-admin-post-editor.php
@@ -47,11 +47,13 @@ class N2_Admin_Post_Editor {
 		$persisted_preferences = get_user_meta( $n2->current_user->ID, "{$n2->blog_prefix}persisted_preferences", true ) ?: array();
 
 		// 設定の強制
-		$persisted_preferences['core/edit-post']['welcomeGuide']               = false;
-		$persisted_preferences['core/edit-post']['showBlockBreadcrumbs']       = false;
-		$persisted_preferences['core/edit-post']['isPublishSidebarEnabled']    = false;
-		$persisted_preferences['core/edit-post']['isComplementaryAreaVisible'] = false;
-		$persisted_preferences['_modified']                                    = gmdate( 'c' );
+		$persisted_preferences['core/edit-post'] = array(
+			'welcomeGuide'               => false,
+			'showBlockBreadcrumbs'       => false,
+			'isPublishSidebarEnabled'    => false,
+			'isComplementaryAreaVisible' => false,
+		);
+		$persisted_preferences['_modified']      = gmdate( 'c' );
 		update_user_meta( $n2->current_user->ID, "{$n2->blog_prefix}persisted_preferences", $persisted_preferences );
 
 		$supports = array(
