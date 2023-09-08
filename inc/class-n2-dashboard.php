f8882a3c9977f1c72474f9c8e562abc8cbf439c2
yamasaki-hiroyoshi <hiroyoshi.yamasaki@steamship.co.jp>
2023-09-08 17:49:03 +0900

    自治体確認ステータスと却下理由の表示

--- inc/class-n2-dashboard.php
+++ inc/class-n2-dashboard.php
@@ -125,8 +125,8 @@ class N2_Dashboard {
 			'post_type'      => 'post',
 			'posts_per_page' => -1,
 			'orderby'        => 'meta_value',
-			'meta_key'       => '返礼品コード',
-			'order'          => 'ASC',
+			'meta_key'       => '自治体確認',
+			'order'          => 'DESC',
 		);
 
 		$wp_query = new WP_Query( $args );
@@ -134,7 +134,7 @@ class N2_Dashboard {
 		$return_rate_list_text = '';
 		if ( $wp_query->have_posts() ) {
 			$return_rate_list_text .= '<ul>';
-			$return_rate_list_text .= '<li style="border-bottom:1px solid #bbb; background:#ccc;display:flex;padding:10px 0;"><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">ステータス</span><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">返礼品コード</span><span style="display:inline-block;width:100%;text-align:center;">返礼品名</span></li>';
+			$return_rate_list_text .= '<li style="border-bottom:1px solid #bbb; background:#ccc;display:flex;padding:10px 0;"><span style="display:inline-block; width:40px;flex-shrink: 0;text-align:center;">確認</span><span style="display:inline-block; width:60px;flex-shrink: 0;text-align:center;">登録</span><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">返礼品コード</span><span style="display:inline-block;width:120px;flex-shrink: 0;text-align:center;">返礼品名</span><span style="display:inline-block;width:100%;text-align:center;">却下理由</span></li>';
 			while ( $wp_query->have_posts() ) {
 				$wp_query->the_post();
 				$post            = get_post( get_the_ID() );
@@ -144,6 +144,7 @@ class N2_Dashboard {
 				$donation_amount = ! empty( $post_data['寄附金額'] ) && 0 !== $post_data['寄附金額'] ? number_format( $post_data['寄附金額'] ) : '-';
 				$code            = ! empty( $post_data['返礼品コード'] ) ? $post_data['返礼品コード'] : '未(id:' . $post->ID . ')';
 				$jichitai_check  = ! empty( $post_data['自治体確認'] ) ? $post_data['自治体確認'] : '-';
+				$reject_reason   = ! empty( $post_data['却下理由'] ) ? $post_data['却下理由'] : '-';
 				$post_status     = $post->post_status;
 				$return_rate     = N2_Donation_Amount_API::calc_return_rate( $post_data ); // 返礼率計算
 				if ( 'draft' === $post_status ) {
@@ -151,14 +152,14 @@ class N2_Dashboard {
 				} elseif ( 'pending' === $post_status ) {
 					$post_status_name = 'SS確認待ち';
 				} elseif ( 'publish' === $post_status ) {
-					$post_status_name = '登録準備中';
+					$post_status_name = '準備中';
 				} elseif ( 'registered' === $post_status ) {
-					$post_status_name = '登録済';
+					$post_status_name = '済';
 				} else {
 					$post_status_name = '未登録';
 				}
-				if ( '承諾' !== $jichitai_check && ('publish' === $post_status || 'registered' === $post_status) ) {
-					$return_rate_list_text .= '<li style="border-bottom:1px solid #ccc;padding:5px 0;"><a href="' . $post_edit_url . '" style="display:flex;"><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">' . $post_status_name . '</span><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">' . $code . '</span><span style="display:inline-block;">' . get_the_title() . '</span></a></li>';
+				if ( '承諾' !== $jichitai_check && ( 'publish' === $post_status || 'registered' === $post_status ) ) {
+					$return_rate_list_text .= '<li style="border-bottom:1px solid #ccc;padding:5px 0;"><a href="' . $post_edit_url . '" style="display:flex;"><span style="display:inline-block; width:40px;flex-shrink: 0;text-align:center;">' . $jichitai_check . '</span><span style="display:inline-block; width:60px;flex-shrink: 0;text-align:center;">' . $post_status_name . '</span><span style="display:inline-block; width:100px;flex-shrink: 0;text-align:center;">' . $code . '</span><span style="display:inline-block;width:120px;flex-shrink: 0;">' . get_the_title() . '</span><span style="display:inline-block;">' . $reject_reason . '</span></a></li>';
 				}
 			}
 			$return_rate_list_text .= '</ul>';
