7c5b9e7a8e50078808efb6a2f2b4f2e95118bdcd
hirotoki3193 <1stlevi777@gmail.com>
2023-10-23 14:56:05 +0900

    :+1:[add] ItemAPI爆誕

---
+++ inc/api/class-n2-rms-item-api.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * RMS ITEM API
+ * /wp-admin/admin-ajax.php?action=n2_rms_item_api_ajax&mode=debug&call=
+ *
+ * @package neoneng
+ */
+
+if ( class_exists( 'N2_RMS_Item_API' ) ) {
+	new N2_RMS_Item_API();
+	return;
+}
+
+/**
+ * N2からCABINETへ送信したりするAPI
+ */
+class N2_RMS_Item_API extends N2_RMS_Base_API {
+
+	/**
+	 * この機能を利用すると、商品管理番号を指定し、商品情報の部分更新をすることができます。
+	 * リクエストに含まれない項目は更新対象にならないため、指定した項目のみ更新されます。
+	 *
+	 * @param string $manageNumber manageNumber
+	 */
+	public static function items_patch( $manageNumber, $body ) {
+		static::check_fatal_error(
+			preg_match( '/[^a-Z0-9\-\_]/', $manageNumber ),
+			'商品管理番号は英数字と「-」「_」のみ使用可能です'
+		);
+		$url = static::$settings['indpoint'] . '/2.0/items/manage-numbers/' . $manageNumber;
+		static::request(
+			$url,
+			array(
+				'method' => 'PATCH',
+				'body'   => $body,
+			),
+		);
+	}
+}
