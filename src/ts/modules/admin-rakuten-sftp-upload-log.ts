3905a3af692873d1582a397ee60e267dfea0c913
hirotoki3193 <1stlevi777@gmail.com>
2023-10-25 11:38:16 +0900

    :tada:[add] アップデートログのテーブル追加

--- src/ts/modules/admin-rakuten-sftp-upload-log.ts
+++ src/ts/modules/admin-rakuten-sftp-upload-log.ts
@@ -5,6 +5,30 @@ export default Vue.extend({
 	async created(){
 		await this.updateSFTPLog();
 	},
+	data() {
+		return {
+			tr: {
+				id: {
+					name:'ID'
+				},
+				upload_type: {
+					name: '転送モード',
+					value:{
+						img_upload: '商品画像',
+						csv_upload: '商品CSV',
+					},
+				},
+				upload_log: {
+					name:'ログ',
+					details: true,
+				},
+				upload_data: {
+					name:'データ',
+					details: true,
+				},
+			}
+		};
+	},
 	computed:{
 		...mapState([
 			'sftpLog'
@@ -16,13 +40,37 @@ export default Vue.extend({
 		]),
 	},
 	template:`
-	<div>
-		<template v-if="sftpLog?.items?.length">
-			sftp
-		</template>
-		<template v-else>
-			アップロードログはありません。
-		</template>
-	</div>
+	<table class="table align-middle lh-1">
+		<thead>
+			<tr>
+				<th v-for="th in tr">{{th.name}}</th>
+			</tr>
+		</thead>
+		<tbody>
+			<template v-if="sftpLog.items.length">
+			<tr v-for="item in sftpLog.items">
+				<td v-for="(td,meta) in tr">
+					<template v-if="td?.details">
+						<button type="button" :popovertarget="meta + item.id" class="button button-primary">詳細</button>
+						<div popover="auto" :id="meta + item.id" style="width: 80%; max-height: 80%; overflow-y: scroll;">
+							<pre>{{item[meta]}}</pre>
+						</div>
+					</template>
+					<template v-else-if="td.value">
+						{{td.value[item[meta]]}}
+					</template>
+					<template v-else>
+						{{item[meta]}}
+					</template>
+				</td>
+			</tr>
+			</template>
+			<template v-else>
+			<tr>
+				<td :colspan="Object.keys(tr).length">アップロードログはありません</td>
+			</tr>
+			</template>
+		</tbody>
+	</table>
 	`,
 });
