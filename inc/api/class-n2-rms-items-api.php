3f394d6e5589edc7be516499035c59da636695b7
hirotoki3193 <1stlevi777@gmail.com>
2023-11-09 14:09:04 +0900

    :+1:[add] items.patchとitems.getも追加

--- inc/api/class-n2-rms-items-api.php
+++ inc/api/class-n2-rms-items-api.php
@@ -51,4 +51,47 @@ class N2_RMS_Items_API extends N2_RMS_Base_API {
 
 		return json_decode( $data['body'], true );
 	}
+
+	/**
+	 * RMS WEB SERVICE : items.patch
+	 * この機能を利用すると、商品管理番号を指定し、商品情報の部分更新をすることができます。
+	 * リクエストに含まれない項目は更新対象にならないため、指定した項目のみ更新されます。
+	 * https://webservice.rms.rakuten.co.jp/merchant-portal/view/ja/common/1-1_service_index/itemapi2/partiallyupdateitem/
+	 *
+	 * @param string $manageNumber manageNumber
+	 * @param string $body         body
+	 */
+	public static function items_patch( $manageNumber, $body ) {
+		static::check_fatal_error(
+			preg_match( '/^[a-zA-Z0-9\-\_]*$/', $manageNumber ),
+			'商品管理番号は英数字と「-」「_」のみ使用可能です'
+		);
+		$url = static::$settings['endpoint'] . '/2.0/items/manage-numbers/' . $manageNumber;
+		return static::request(
+			$url,
+			array(
+				'method'  => 'PATCH',
+				'body'    => wp_unslash( $body ),
+				'headers' => array(
+					'Content-Type' => 'application/json',
+				),
+			),
+		);
+	}
+
+	/**
+	 * RMS WEB SERVICE : items.get
+	 * この機能を利用すると、商品管理番号を指定し、商品情報を取得することができます。
+	 * https://webservice.rms.rakuten.co.jp/merchant-portal/view/ja/common/1-1_service_index/itemapi2/getitem/
+	 *
+	 * @param string $manageNumber manageNumber
+	 */
+	public static function items_get( $manageNumber ) {
+		static::check_fatal_error(
+			preg_match( '/^[a-zA-Z0-9\-\_]*$/', $manageNumber ),
+			'商品管理番号は英数字と「-」「_」のみ使用可能です'
+		);
+		$url = static::$settings['endpoint'] . '/2.0/items/manage-numbers/' . $manageNumber;
+		return json_decode( static::request( $url )['body'], true );
+	}
 }
