dbe5182ad00433f7b6f7592fc0167dbee2ddeb2a
ryota-nakano <rnakanor@gmail.com>
2023-11-09 15:17:46 +0900

    :+1: RMSAPI店舗詳細情報追加

--- inc/api/class-n2-rms-shop-api.php
+++ inc/api/class-n2-rms-shop-api.php
@@ -19,13 +19,25 @@ if ( class_exists( 'N2_RMS_Shop_API' ) ) {
 class N2_RMS_Shop_API extends N2_RMS_Base_API {
 
 	/**
+	 * 店舗詳細情報を取得
+	 *
+	 * @return object
+	 */
+	public static function shop_master_get() {
+		$url    = static::$settings['endpoint'] . '/1.0/shop/shopMaster';
+		$data   = wp_remote_get( $url, array( 'headers' => static::$data['header'] ) );
+		$result = (array) simplexml_load_string( $data['body'] )->result->shopMaster;
+		return $result;
+	}
+
+	/**
 	 * 納期情報を取得
-	 * 
+	 *
 	 * @return object
 	 */
 	public static function delvdate_master_get() {
-		$url = static::$settings['endpoint'] . '/1.0/shop/delvdateMaster';
-		$data    = wp_remote_get( $url, array( 'headers' => static::$data['header'] ) );
+		$url    = static::$settings['endpoint'] . '/1.0/shop/delvdateMaster';
+		$data   = wp_remote_get( $url, array( 'headers' => static::$data['header'] ) );
 		$result = (array) simplexml_load_string( $data['body'] )->result->delvdateMasterList;
 		$master = $result['delvdateMaster'];
 		usort( $master, fn( $a, $b ) => (int) $a->delvdateNumber > (int) $b->delvdateNumber ? 1 : -1 );
