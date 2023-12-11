db37ab64a6da574ac8044ed439aa7f43ab1527cd
HiroakiKatada <hiroaki.katada@steamship.co.jp>
2023-12-11 16:07:11 +0900

    :wrench:[fix]左辺がnullである場合はなく、空文字or数値になるのに合わせて修正

--- inc/class-n2-sync.php
+++ inc/class-n2-sync.php
@@ -1132,7 +1132,8 @@ class N2_Sync {
 			}
 			// 送料
 			if ( isset( $d['送料'] ) ) {
-				$d['送料'] = preg_replace( '/[^0-9]/', '', mb_convert_kana( $d['送料'], 'n' ) ) ?? 0;
+				$d['送料'] = preg_replace( '/[^0-9]/', '', mb_convert_kana( $d['送料'], 'n' ) );
+				$d['送料'] = '' === $d['送料'] ? 0 : $d['送料'];
 				if ( empty( $d['送料'] ) && ! empty( $d['発送サイズ'] ) && ! empty( $d['発送方法'] ) ) {
 					$delivery_code = N2_Donation_Amount_API::create_delivery_code( $d['発送サイズ'], $d['発送方法'] );
 					// 送料計算
