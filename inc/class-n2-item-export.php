cc620429ee9f70825de0763cc613a6f4918e2e61
ryota-nakano <rnakanor@gmail.com>
2023-12-12 11:54:03 +0900

    :+1: class-n2-item-export.php調整＆フック追加（プラグインで独自のファイルを追加するため）

--- inc/class-n2-item-export.php
+++ inc/class-n2-item-export.php
@@ -19,24 +19,27 @@ class N2_Item_Export {
 	 */
 	public function __construct() {
 		// エクスポート関連全読込
-		// $path = get_theme_file_path( '/inc/item-export/*.php' );
-		// foreach ( glob( $path ) as $filename ) {
-		// 	require_once $filename;
-		// }
-		$path    = '/inc/item-export/';
-		$exports = array(
-			'class-n2-item-export-base.php',
-			'class-n2-item-export-furusato-choice.php',
-			'class-n2-item-export-ledghome.php',
-			'class-n2-item-export-lhcloud.php',
-			'class-n2-item-export-rakuten.php',
-			'class-n2-item-export-rakuten-select.php',
-			'class-n2-item-export-rakuten-sku.php',
-			'class-n2-item-export-rakuten-cat.php',
-			'class-n2-product-list-print.php',
+		$files = array(
+			'class-n2-item-export-base',
+			'class-n2-item-export-furusato-choice',
+			'class-n2-item-export-ledghome',
+			'class-n2-item-export-lhcloud',
+			'class-n2-item-export-rakuten',
+			'class-n2-item-export-rakuten-select',
+			'class-n2-item-export-rakuten-sku',
+			'class-n2-item-export-rakuten-cat',
+			'class-n2-product-list-print',
 		);
-		foreach ( $exports as $export ) {
-			require_once get_theme_file_path( $path . $export );
+		// フルパス生成
+		$files = array_map( fn( $v ) => get_theme_file_path( "/inc/item-export/{$v}.php" ), $files );
+		/**
+		 * [hook] n2_item_export_files
+		 */
+		$files = apply_filters( 'n2_item_export_files', $files );
+
+		// $filesを全部読み込み
+		foreach ( $files as $file ) {
+			require_once $file;
 		}
 	}
 }
