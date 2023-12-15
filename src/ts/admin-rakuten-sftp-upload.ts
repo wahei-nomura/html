7f1d4b0a0d6f042153e5f7dcd950b2cfb55e4c1b
hirotoki3193 <1stlevi777@gmail.com>
2023-12-15 15:48:12 +0900

    :wrench:[fix] シンプルに

--- src/ts/admin-rakuten-sftp-upload.ts
+++ src/ts/admin-rakuten-sftp-upload.ts
@@ -5,6 +5,6 @@ jQuery(function($){
 		if ( ! fileInput.files.length ) return;
 		console.log(fileInput.files);
 		const hasDeleteCSV = Array.from(fileInput.files).filter(file=>file.name.indexOf( 'item-delete') !== -1 ).length;
-		if ( hasDeleteCSV && ! confirm( '商品情報削除用CSVが選択されています。続けますか？' ) ) fileInput.value = null;
+		if ( hasDeleteCSV && ! confirm( 'item-delete.csv が選択されています。続けますか？' ) ) fileInput.value = null;
 	})
 })
