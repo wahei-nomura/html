1d83bef2987b0243bbc3b4bc3bcefd25ae928a36
ryota-nakano <rnakanor@gmail.com>
2023-12-19 18:11:40 +0900

    :bug: 楽天SKU田代だるすぎ

--- inc/api/class-n2-items-api-rakuten.php
+++ inc/api/class-n2-items-api-rakuten.php
@@ -64,7 +64,7 @@ class N2_Items_API_Rakuten extends N2_Portal_Item_Data {
 	 */
 	public function array_format( $v ) {
 		$item = $v['item'];
-		$ids  = array( $item['itemNumber'], $item['manageNumber'], 'normal-inventory' );
+		$ids  = array( (string) $item['itemNumber'], (string) $item['manageNumber'], 'normal-inventory' );
 		return array(
 			'goods_g_num' => $item['itemNumber'],
 			'goods_name'  => $item['title'],
