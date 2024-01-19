2ea281891f67302ccba4bc6b448d6bf1845591ea
SuperKojima <hayato.kojima@steamship.co.jp>
2024-01-19 17:53:10 +0900

    vueのインポートをesmでエイリアス貼った

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-const fs                   = require( "node:fs" );
-const path                 = require( "path" );
-const glob                 = require( 'glob' );
-const RemoveEmptyScriptsPlugin = require( "webpack-remove-empty-scripts" );// scssのコンパイルのみの際のゴミjs削除
-const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
-const CssMinimizerPlugin   = require( "css-minimizer-webpack-plugin" );
+const fs = require("node:fs");
+const path = require("path");
+const glob = require("glob");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts"); // scssのコンパイルのみの際のゴミjs削除
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 // dist ディレクトリごと初期化
 const rmDir = (dirPath) => {
-	if ( !fs.existsSync(dirPath) ) { return }
+	if (!fs.existsSync(dirPath)) {
+		return;
+	}
 	const items = fs.readdirSync(dirPath);
-	for ( const item of items ) {
-		const deleteTarget = path.join( dirPath, item );
-		if ( fs.lstatSync(deleteTarget).isDirectory() ) {
+	for (const item of items) {
+		const deleteTarget = path.join(dirPath, item);
+		if (fs.lstatSync(deleteTarget).isDirectory()) {
 			rmDir(deleteTarget);
 		} else {
 			fs.unlinkSync(deleteTarget);
 		}
 	}
-	fs.rmdirSync( dirPath );
-}
-rmDir( path.resolve(__dirname, 'dist') );
-console.log('distを削除');
+	fs.rmdirSync(dirPath);
+};
+rmDir(path.resolve(__dirname, "dist"));
+console.log("distを削除");
 // entry作成
-const base_path    = path.join(__dirname, 'src');
+const base_path = path.join(__dirname, "src");
 /**
- * 
+ *
  * @param {string} src 対象ディレクトリ
  * @param {string} reg 拡張子正規表現
- * @returns 
+ * @returns
  */
 const entry_arr = (src, reg) => {
 	const entry = {};
-	glob.sync(`${path.join(__dirname, src)}/*.+(${reg})`).forEach(value => {
-		entry[ path.basename( value, path.extname(value) ) ] = value;
+	glob.sync(`${path.join(__dirname, src)}/*.+(${reg})`).forEach((value) => {
+		entry[path.basename(value, path.extname(value))] = value;
 	});
 	return entry;
 };
@@ -40,10 +42,10 @@ const entry_arr = (src, reg) => {
 // jsのコンパイル
 const js = {
 	mode: "development",
-	entry: entry_arr('src/ts', 'ts|js'),
+	entry: entry_arr("src/ts", "ts|js"),
 	output: {
-		path: path.resolve(__dirname, 'dist/js'),
-		filename: '[name].js',
+		path: path.resolve(__dirname, "dist/js"),
+		filename: "[name].js",
 	},
 	module: {
 		rules: [
@@ -55,6 +57,10 @@ const js = {
 	},
 	resolve: {
 		extensions: [".ts", ".js"],
+		alias: {
+			// import Vue from 'vue'; と記述したときの 'vue' が表すファイルパスを設定
+			vue$: "vue/dist/vue.esm.js",
+		},
 	},
 	// devtool: 'source-map',
 };
@@ -62,9 +68,9 @@ const js = {
 // css
 const css = {
 	mode: "development",
-	entry: entry_arr('src/scss', 'scss|sass|css'),
+	entry: entry_arr("src/scss", "scss|sass|css"),
 	output: {
-		path: path.resolve(__dirname, 'dist/css'),
+		path: path.resolve(__dirname, "dist/css"),
 	},
 	module: {
 		rules: [
@@ -89,15 +95,13 @@ const css = {
 		extensions: [".scss", ".sass", ".css"],
 	},
 	plugins: [
-		new MiniCssExtractPlugin(
-			{
-				filename: '[name].css',
-			}
-		),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		}),
 		new CssMinimizerPlugin(),
 		new RemoveEmptyScriptsPlugin(),
 	],
 	// devtool: 'source-map',
 };
 
-module.exports = [js, css]
+module.exports = [js, css];
