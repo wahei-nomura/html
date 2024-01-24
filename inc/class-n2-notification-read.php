ea45b1290c98c8044d2d6c6c17d082e2ddda3602
SuperKojima <hayato.kojima@steamship.co.jp>
2024-01-24 14:39:38 +0900

    リダイレクト設定

--- inc/class-n2-notification-read.php
+++ inc/class-n2-notification-read.php
@@ -18,25 +18,35 @@ class N2_Notification_Read {
 	 * コンストラクタ
 	 */
 	public function __construct() {
-		if (get_site()->blog_id != 1) {
-			// 自治体のページならナビゲーションに追加
-			add_action( 'admin_menu', array( $this, 'add_menu' ) );
-		}
+		add_action( 'admin_init', array($this, 'custom_redirect_on_permission_error'));
+		add_action( 'admin_menu', array( $this, 'add_menu' ) );
 	}
 
 	/**
 	 * メニュー追加
 	 */
 	public function add_menu() {
-		add_menu_page(
-			'お知らせ',
-			'お知らせ',
-			'ss_crew',
-			'n2_notification_read',
-			[$this, 'display_page'],
-			'dashicons-bell',
-			25
-		);
+		// 自治体のページならナビゲーションに追加
+		if (get_site()->blog_id != 1) {
+			add_menu_page(
+				'お知らせ',
+				'お知らせ',
+				'ss_crew',
+				'n2_notification_read',
+				[$this, 'display_page'],
+				'dashicons-bell',
+				25
+			);
+		} else {
+			wp_redirect('edit.php?post_type=notification');
+		}
+	}
+
+	public function custom_redirect_on_permission_error() {
+		// メインサイトには閲覧用ページがないから管理用のページにリダイレクト
+		if (get_site()->blog_id == 1) {
+			wp_redirect('/wp-admin/edit.php?post_type=notification');
+		}
 	}
 
 	public function display_page() {
